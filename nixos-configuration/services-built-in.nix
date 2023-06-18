c85545646a80a4e4a2665aec9b731b57f1d3e35a
Pratham Patel <thefirst1322@gmail.com>
2023-06-18 17:51:32 +0530

    ensure port 22 is open for SSH

--- nixos-configuration/services-built-in.nix
+++ nixos-configuration/services-built-in.nix
@@ -9,6 +9,8 @@
     openssh = {
       enable = true;
       extraConfig = "PermitEmptyPasswords no";
+      ports = [ 22 ];
+      openFirewall = true;
       settings = {
         PasswordAuthentication = false;
         PermitRootLogin = "prohibit-password";
