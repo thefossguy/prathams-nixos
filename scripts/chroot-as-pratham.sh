7dce967231a23edb0c419e7e381ff3015f6b1565
Pratham Patel <thefirst1322@gmail.com>
2023-08-26 16:01:21 +0530

    chroot script: clone the nixos config too

--- scripts/chroot-as-pratham.sh
+++ scripts/chroot-as-pratham.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i dash --packages dash bash git
 
-set -x
+set -xeu
 if [ "${USER}" = 'pratham' ] && [ -n "${HOME}" ]; then
     # get dotfiles
     git clone --bare https://gitlab.com/thefossguy/dotfiles.git "${HOME}/.dotfiles"
@@ -9,11 +9,17 @@ if [ "${USER}" = 'pratham' ] && [ -n "${HOME}" ]; then
 
     # generate SSH keys
     chmod 700 "${HOME}/.ssh"
-    pushd "${HOME}/.ssh" || exit 1
+    pushd "${HOME}/.ssh"
     clear -x
     ssh-keygen -t ed25519 -f git
     ssh-keygen -t ed25519 -f ssh
-    popd || exit 0
+    popd
+
+    # get nixos config
+    mkdir -vp "${HOME}/my-git-repos/pratham"
+    pushd "${HOME}/my-git-repos/pratham"
+    git clone https://git.thefossguy.com/thefossguy/prathams-nixos
+    popd
 else
     >&2 echo "$0: You are not me"
     exit 1
