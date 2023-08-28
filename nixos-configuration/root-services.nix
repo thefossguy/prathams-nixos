33d5dea1563ddfbd35ab7885f3dd9a6bea633fbf
Pratham Patel <thefirst1322@gmail.com>
2023-08-28 15:07:39 +0530

    add systemd service for root to copy nixos config files from git repo into /etc/nixos

--- nixos-configuration/root-services.nix
+++ nixos-configuration/root-services.nix
@@ -29,4 +29,41 @@
       };
     };
   };
+
+  systemd.timers = {
+    "update-nixos-config" = {
+      description = "Timer to update NixOS configuration";
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "*-*-* 23:30:00";
+        Unit = "update-nixos-config.service";
+      };
+    };
+  };
+  systemd.services = {
+    "update-nixos-config" = {
+      description = "Service to update NixOS configuration";
+      documentation = [ "man:cp(1)" ];
+      script = ''
+        set -xeu
+        NIXOS_CONFIG_REPO='/home/pratham/my-git-repos/pratham/prathams-nixos'
+
+        if [ "$(id -u)" -ne 0 ]; then
+            >&2 echo "$0: please run this script as root"
+            exit 1
+        fi
+
+        if [ ! -d "$NIXOS_CONFIG_REPO/.git" ]; then
+            >&2 echo "$0: '$NIXOS_CONFIG_REPO' does not exist..."
+            exit 1
+        fi
+
+        cp -fR "$NIXOS_CONFIG_REPO"/nixos-configuration/* /etc/nixos
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+      };
+    };
+  };
 }
