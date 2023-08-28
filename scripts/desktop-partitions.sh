a23fdff1da7fac7c354ca065b4e0f0fd1ac74d17
Pratham Patel <thefirst1322@gmail.com>
2023-08-28 13:29:04 +0530

    update disk partitioning
    1. Add a separate mount for '/nix'
    2. Resize '/boot' and `/`, both to 512M
    3. For Raspberry Pi partitioning script, move 'parted' commands after
       the 'mkfs.fat' command is run, like done in 'scripts/{desktop,virt}-partitions.sh'
    4. Mount '/nix' with option 'noatime'

--- scripts/desktop-partitions.sh
+++ scripts/desktop-partitions.sh
@@ -4,9 +4,11 @@
 set -x
 BOOT_PART="${INTERMEDIATE_PART}1"
 ROOT_PART="${INTERMEDIATE_PART}2"
-HOME_PART="${INTERMEDIATE_PART}3"
-BOOT_PART_SIZE='1G'
-ROOT_PART_SIZE='128G'
+STRG_PART="${INTERMEDIATE_PART}3"
+HOME_PART="${INTERMEDIATE_PART}4"
+BOOT_PART_SIZE='512M'
+ROOT_PART_SIZE='512M'
+STRG_PART_SIZE='128G'
 
 cat << EOF | fdisk --wipe always "${OS_DRIVE}"
 g
@@ -21,6 +23,10 @@ n
 n
 3
 
++${STRG_PART_SIZE}
+n
+4
+
 
 w
 EOF
@@ -33,8 +39,10 @@ fdisk -l "${OS_DRIVE}"
 mkfs.fat  -F 32 -n nixboot "${BOOT_PART}"
 parted -s "${OS_DRIVE}" -- set 1 esp on
 mkfs.ext4 -F -L    nixroot "${ROOT_PART}"
+mkfs.ext4 -F -L    nixstrg "${STRG_PART}"
 mkfs.ext4 -F -L    nixhome "${HOME_PART}"
 
-mount         "${ROOT_PART}" "${MOUNT_PATH}"
-mount --mkdir "${BOOT_PART}" "${MOUNT_PATH}/boot"
-mount --mkdir "${HOME_PART}" "${MOUNT_PATH}/home"
+mount                    "${ROOT_PART}" "${MOUNT_PATH}"
+mount --mkdir            "${BOOT_PART}" "${MOUNT_PATH}/boot"
+mount --mkdir -o noatime "${STRG_PART}" "${MOUNT_PATH}/nix"
+mount --mkdir            "${HOME_PART}" "${MOUNT_PATH}/home"
--- scripts/raspberry-pi-partitions.sh
+++ scripts/raspberry-pi-partitions.sh
@@ -5,11 +5,13 @@ set -x
 RASP_PART="${INTERMEDIATE_PART}1"
 BOOT_PART="${INTERMEDIATE_PART}2"
 ROOT_PART="${INTERMEDIATE_PART}3"
-HOME_PART="${INTERMEDIATE_PART}4"
-VARL_PART="${INTERMEDIATE_PART}5"
+STRG_PART="${INTERMEDIATE_PART}4"
+HOME_PART="${INTERMEDIATE_PART}5"
+VARL_PART="${INTERMEDIATE_PART}6"
 RASP_PART_SIZE='64M'
-BOOT_PART_SIZE='1G'
-ROOT_PART_SIZE='80G'
+BOOT_PART_SIZE='512M'
+ROOT_PART_SIZE='512M'
+STRG_PART_SIZE='80G'
 HOME_PART_SIZE='32G'
 
 cat << EOF | fdisk --wipe always "${OS_DRIVE}"
@@ -29,9 +31,13 @@ n
 n
 4
 
-+${HOME_PART_SIZE}
++${STRG_PART_SIZE}
 n
 5
+
++${HOME_PART_SIZE}
+n
+6
 
 
 t
@@ -42,8 +48,6 @@ t
 0b
 w
 EOF
-parted -s "${OS_DRIVE}" -- set 1 esp on
-parted -s "${OS_DRIVE}" -- set 2 esp on
 sync; sync; sync; sync;
 sleep 10
 sync; sync; sync; sync;
@@ -52,15 +56,19 @@ fdisk -l "${OS_DRIVE}"
 
 mkfs.fat  -F 32 -n pftf    "${RASP_PART}"
 mkfs.fat  -F 32 -n nixboot "${BOOT_PART}"
+parted -s "${OS_DRIVE}" -- set 1 esp on
+parted -s "${OS_DRIVE}" -- set 2 esp on
 mkfs.ext4 -F -L    nixroot "${ROOT_PART}"
+mkfs.ext4 -F -L    nixstrg "${STRG_PART}"
 mkfs.ext4 -F -L    nixhome "${HOME_PART}"
 mkfs.ext4 -F -L    nixvarp "${VARL_PART}"
 
-mount         "${ROOT_PART}" "${MOUNT_PATH}"
-mount --mkdir "${BOOT_PART}" "${MOUNT_PATH}/boot"
-mount --mkdir "${HOME_PART}" "${MOUNT_PATH}/home"
-mount --mkdir "${VARL_PART}" "${MOUNT_PATH}/var"
-mount --mkdir "${RASP_PART}" "${MOUNT_PATH}/raspberry-pi/firmware"
+mount                    "${ROOT_PART}" "${MOUNT_PATH}"
+mount --mkdir            "${BOOT_PART}" "${MOUNT_PATH}/boot"
+mount --mkdir -o noatime "${STRG_PART}" "${MOUNT_PATH}/nix"
+mount --mkdir            "${HOME_PART}" "${MOUNT_PATH}/home"
+mount --mkdir            "${VARL_PART}" "${MOUNT_PATH}/var"
+mount --mkdir            "${RASP_PART}" "${MOUNT_PATH}/raspberry-pi/firmware"
 
 "$(pwd)/scripts/get-raspi-4-firmware.sh"
 cp -r "$(pwd)"/out/* "${MOUNT_PATH}/raspberry-pi/firmware"
