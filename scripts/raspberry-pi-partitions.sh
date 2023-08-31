d5cc2cc9017f24c23d73eb1952fa1f416b69c84a
Pratham Patel <thefirst1322@gmail.com>
2023-08-31 11:01:50 +0530

    partially revert a23fdff1da7fac7c354ca065b4e0f0fd1ac74d17
    When `nixos-build`-ing, Podman gets built on '/', strange but what I saw
    that with `watch -n 1 'df -kh'`, the 512M root partition would fill up.
    
    So undo the decision of a 512M '/' and "variable sized" '/nix' and 512M
    '/boot'. The rest is the same, for what it is worth.

--- scripts/raspberry-pi-partitions.sh
+++ scripts/raspberry-pi-partitions.sh
@@ -5,13 +5,11 @@ set -xe
 RASP_PART="${INTERMEDIATE_PART}1"
 BOOT_PART="${INTERMEDIATE_PART}2"
 ROOT_PART="${INTERMEDIATE_PART}3"
-STRG_PART="${INTERMEDIATE_PART}4"
-HOME_PART="${INTERMEDIATE_PART}5"
-VARL_PART="${INTERMEDIATE_PART}6"
+HOME_PART="${INTERMEDIATE_PART}4"
+VARL_PART="${INTERMEDIATE_PART}5"
 RASP_PART_SIZE='64M'
-BOOT_PART_SIZE='512M'
-ROOT_PART_SIZE='512M'
-STRG_PART_SIZE='80G'
+BOOT_PART_SIZE='1G'
+ROOT_PART_SIZE='80G'
 HOME_PART_SIZE='32G'
 
 cat << EOF | fdisk --wipe always "${OS_DRIVE}"
@@ -31,13 +29,9 @@ n
 n
 4
 
-+${STRG_PART_SIZE}
-n
-5
-
 +${HOME_PART_SIZE}
 n
-6
+5
 
 
 t
@@ -52,24 +46,22 @@ sync; sync; sync; sync;
 sleep 10
 sync; sync; sync; sync;
 
-fdisk -l "${OS_DRIVE}"
-
 mkfs.fat  -F 32 -n pftf    "${RASP_PART}"
 mkfs.fat  -F 32 -n nixboot "${BOOT_PART}"
 parted -s "${OS_DRIVE}" -- set 1 esp on
 parted -s "${OS_DRIVE}" -- set 2 esp on
 mkfs.ext4 -F -L    nixroot "${ROOT_PART}"
-mkfs.ext4 -F -L    nixstrg "${STRG_PART}"
 mkfs.ext4 -F -L    nixhome "${HOME_PART}"
 mkfs.ext4 -F -L    nixvarp "${VARL_PART}"
 
-mount                    "${ROOT_PART}" "${MOUNT_PATH}"
-mount --mkdir            "${BOOT_PART}" "${MOUNT_PATH}/boot"
-mount --mkdir -o noatime "${STRG_PART}" "${MOUNT_PATH}/nix"
-mount --mkdir            "${HOME_PART}" "${MOUNT_PATH}/home"
-mount --mkdir            "${VARL_PART}" "${MOUNT_PATH}/var"
-mount --mkdir            "${RASP_PART}" "${MOUNT_PATH}/raspberry-pi/firmware"
+mount -o noatime "${ROOT_PART}" "${MOUNT_PATH}"
+mount --mkdir    "${BOOT_PART}" "${MOUNT_PATH}/boot"
+mount --mkdir    "${HOME_PART}" "${MOUNT_PATH}/home"
+mount --mkdir    "${VARL_PART}" "${MOUNT_PATH}/var"
+mount --mkdir    "${RASP_PART}" "${MOUNT_PATH}/raspberry-pi/firmware"
 
 "$(pwd)/scripts/get-raspi-4-firmware.sh"
 cp -r "$(pwd)"/out/* "${MOUNT_PATH}/raspberry-pi/firmware"
 umount "${MOUNT_PATH}/raspberry-pi/firmware"
+
+fdisk -l "${OS_DRIVE}"
--- scripts/virt-partitions.sh
+++ scripts/virt-partitions.sh
@@ -4,11 +4,9 @@
 set -xe
 BOOT_PART="${INTERMEDIATE_PART}1"
 ROOT_PART="${INTERMEDIATE_PART}2"
-STRG_PART="${INTERMEDIATE_PART}3"
-HOME_PART="${INTERMEDIATE_PART}4"
-BOOT_PART_SIZE='512M'
-ROOT_PART_SIZE='512M'
-STRG_PART_SIZE='48G'
+HOME_PART="${INTERMEDIATE_PART}3"
+BOOT_PART_SIZE='1G'
+ROOT_PART_SIZE='48G'
 
 cat << EOF | fdisk --wipe always "${OS_DRIVE}"
 g
@@ -23,10 +21,6 @@ n
 n
 3
 
-+${STRG_PART_SIZE}
-n
-4
-
 
 w
 EOF
@@ -34,15 +28,13 @@ sync; sync; sync; sync;
 sleep 10
 sync; sync; sync; sync;
 
-fdisk -l "${OS_DRIVE}"
-
 mkfs.fat  -F 32 -n nixboot "${BOOT_PART}"
 parted -s "${OS_DRIVE}" -- set 1 esp on
 mkfs.ext4 -F -L    nixroot "${ROOT_PART}"
-mkfs.ext4 -F -L    nixstrg "${STRG_PART}"
 mkfs.ext4 -F -L    nixhome "${HOME_PART}"
 
-mount                    "${ROOT_PART}" "${MOUNT_PATH}"
-mount --mkdir            "${BOOT_PART}" "${MOUNT_PATH}/boot"
-mount --mkdir -o noatime "${STRG_PART}" "${MOUNT_PATH}/nix"
-mount --mkdir            "${HOME_PART}" "${MOUNT_PATH}/home"
+mount -o noatime "${ROOT_PART}" "${MOUNT_PATH}"
+mount --mkdir    "${BOOT_PART}" "${MOUNT_PATH}/boot"
+mount --mkdir    "${HOME_PART}" "${MOUNT_PATH}/home"
+
+fdisk -l "${OS_DRIVE}"
