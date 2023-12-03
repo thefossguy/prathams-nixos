thefossguy/prathams-nixos

commit badd82448762e3f87962e8ccd6875f44ee7c7a91

enable swap if $TOTAL_MEM_GIB is less tahn 4GiB

scripts/deviation.sh
@@ -87,11 +87,19 @@ else
 EOF
 fi
 
-if [ "${TOTAL_MEM_GIB}" -gt 30 ]; then
+if [ "${TOTAL_MEM_GIB}" -lt 4 ]; then
+    cat << EOF >> "${CUSTOM_HOST_CONFIG}"
+
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 8*1024;
+  ]};
+EOF
+elif [ "${TOTAL_MEM_GIB}" -gt 30 ]; then
     cat << EOF >> "${CUSTOM_HOST_CONFIG}"
 
   boot.tmp = {
-    useTmpfs = true; # mount a tmpfs on /tmp during boot
+    useTmpfs = true; # mount the tmpfs on /tmp during boot
     tmpfsSize = "50%";
   };
 EOF

commit fe472618d341d64154c98ea1cb5cce6d90de6fe6

fix typo introduced in badd82448762e3f87962e8ccd6875f44ee7c7a91

scripts/deviation.sh
@@ -93,7 +93,7 @@ if [ "${TOTAL_MEM_GIB}" -lt 4 ]; then
   swapDevices = [{
     device = "/var/lib/swapfile";
     size = 8*1024;
-  ]};
+  }];
 EOF
 elif [ "${TOTAL_MEM_GIB}" -gt 30 ]; then
     cat << EOF >> "${CUSTOM_HOST_CONFIG}"