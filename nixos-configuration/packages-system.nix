b35a7239da7a56bca974278db6dedc720c6a7c22
Pratham Patel <thefirst1322@gmail.com>
2023-12-03 08:45:27 +0530

    remove pkg: linux.dev

--- nixos-configuration/packages-system.nix
+++ nixos-configuration/packages-system.nix
@@ -96,7 +96,6 @@
     cargo-vet # ensure that the third-party dependencies are audited by a trusted source
     cargo-watch # run cargo commands when the src changes
     gcc
-    linux.dev
     rustup # provides rustfmt, cargo-clippy, rustup, cargo, rust-lldb, rust-analyzer, rustc, rust-gdb, cargo-fmt
 
     # language servers, parsers and other related packages
