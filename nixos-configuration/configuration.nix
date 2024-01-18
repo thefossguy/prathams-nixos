04d479b318d134919f4aef7ebfdae7100e8b8fec
Pratham Patel <thefirst1322@gmail.com>
2024-01-18 16:07:16 +0530

    move config options from root-services.nix to configuration.nix

--- nixos-configuration/configuration.nix
+++ nixos-configuration/configuration.nix
@@ -11,9 +11,6 @@
     # users, password-less-sudo and groups config
     ./users-configuration.nix
 
-    # services to enable
-    ./root-services.nix
-
     # specific to this host
     ./host-specific-configuration.nix
 
@@ -31,6 +28,56 @@
       allowReboot = false;
       operation = "boot";
       persistent = true;
+    };
+  };
+
+  services = {
+    fwupd.enable = true;
+    journald.storage = "persistent";
+    logrotate.enable = true;
+    timesyncd.enable = true; # NTP
+    udisks2.enable = true;
+
+    locate = {
+      enable = true;
+      package = pkgs.mlocate;
+      localuser = null;
+      interval = "daily";
+      prunePaths = [ "/nix/store" ];
+    };
+
+    openssh = {
+      enable = true;
+      extraConfig = "PermitEmptyPasswords no";
+      ports = [ 22 ];
+      openFirewall = true;
+      settings = {
+        Protocol = 2;
+        MaxAuthTries = 2;
+        PermitEmptyPasswords = false;
+        PasswordAuthentication = false;
+        PermitRootLogin = "prohibit-password";
+        X11Forwarding = false;
+      };
+    };
+  };
+
+  systemd.timers = {
+    "update-nixos-config" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "*-*-* 23:30:00";
+        Unit = "update-nixos-config.service";
+      };
+    };
+  };
+  systemd.services = {
+    "update-nixos-config" = {
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        ExecStart = "${pkgs.coreutils}/bin/cp -fR /home/pratham/my-git-repos/pratham/prathams-nixos/nixos-configuration/. /etc/nixos";
+      };
     };
   };
 
