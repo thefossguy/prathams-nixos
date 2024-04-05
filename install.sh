dfe877ca1ef158b22833b1b611a26e7fa8a7932f
Pratham Patel <prathampatel@thefossguy.com>
2024-04-05 15:53:53 +0530

    redo installer

--- install.sh
+++ install.sh
@@ -1,153 +1,98 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i bash --packages parted pciutils
-
-# not using 'set -uf' because file globbing is necessary
-# and we will be checking if vars are empty or not, manually
-set -xe -o pipefail
+#!/usr/bin/env bash
 
 date +%Y/%m/%d\ %H:%M:%S
-if [ "$(id -u)" -ne 0 ]; then
-    >&2 echo "$0: please run this script as root"
+set -euf -o pipefail
+
+if [[ "$(id -u)" != '0' ]]; then
+    echo 'ERROR: Please run this script as root'
     exit 1
 fi
 
 if ! ping -c 1 google.com > /dev/null; then
-    >&2 echo "$0: not connected to the internet... exiting..."
+    echo 'ERROR: Not connected to the internet... exiting...'
     exit 1
 fi
 
-if [ -z "${1}" ] || [ -z "${2}" ] || [ -z "${3}" ]; then
-    >&2 echo "$0: missing args for either 'OS drive', 'hostname' or 'disk partition layout'"
+if [[ -z "${1:-}" || -z "${2:-}" ]]; then
+    # shellcheck disable=SC2016
+    echo 'ERROR: Insufficient arguments... $1:hostname, $2:target_disk'
+    exit 1
+else
+    HOSTNAME="$1"
+    TARGET_DRIVE="$2"
+fi
+
+if [ -b "${TARGET_DRIVE}" ]; then
+    if echo "${TARGET_DRIVE}" | grep "sd\|vd" > /dev/null; then
+        INTERMEDIATE_PART="${TARGET_DRIVE}"
+    elif echo "${TARGET_DRIVE}" | grep "mmcblk\|nvme\|loop" > /dev/null; then
+        INTERMEDIATE_PART="${TARGET_DRIVE}p"
+    else
+        echo "ERROR: Unable to decide how to partition '${TARGET_DRIVE}'"
+        exit 1
+    fi
+else
+    echo "ERROR: '${TARGET_DRIVE}' is not a block device"
     exit 1
 fi
 
-if sudo dmesg | grep EFI | grep ' by Das U-Boot'; then
-    CAN_TOUCH_EFI_VARS='false'
+TARGET_DRIVE_SIZE_IN_BYTES="$(blockdev --getsize64 "${TARGET_DRIVE}")"
+TARGET_DRIVE_SIZE_IN_GIB="$(( TARGET_DRIVE_SIZE_IN_BYTES / 1024 / 1024 /1024 ))"
+if [[ "${TARGET_DRIVE_SIZE_IN_GIB}" -lt 32 ]]; then
+    echo 'ERROR: Get a bigger disk'
+    exit 1
+elif [[ "${TARGET_DRIVE_SIZE_IN_GIB}" -lt 64 ]]; then
+    ROOT_PART_SIZE=24
+elif [[ "${TARGET_DRIVE_SIZE_IN_GIB}" -lt 256 ]]; then
+    ROOT_PART_SIZE=64
+elif [[ "${TARGET_DRIVE_SIZE_IN_GIB}" -lt 512 ]]; then
+    ROOT_PART_SIZE=128
 else
-    CAN_TOUCH_EFI_VARS='true'
+    ROOT_PART_SIZE=256
 fi
-if [[ -d '/sys/class/power_supply/BAT0' ]]; then
-    BATTERY_POWERED_DEVICE='true'
-else
-    BATTERY_POWERED_DEVICE='false'
-fi
-# '03' is the class number for Display controllers in PCI ID, so grep for it instead
-if lspci -nn | grep '\[03' | grep -i 'AMD' > /dev/null; then
-    export GPU_AMD='true'
 
-# we have this because 'grep -i ATI' also matches
-# VGA comp**ati**ble controller [...] Red Hat, Inc. Virtio [...] GPU
-elif lspci -nn | grep '\[03' | grep 'ATI' > /dev/null; then
-    export GPU_AMD='true'
-fi
-(lspci -nn | grep '\[03' | grep -i 'Intel' > /dev/null) && export GPU_INTEL='true'
-(lspci -nn | grep '\[03' | grep -i 'Nvidia' > /dev/null) && export GPU_NVIDIA='true'
-if [[ "${GPU_AMD}" == 'true' || "${GPU_INTEL}" == 'true' ]]; then
-    export OSS_GPU_DRIVERS='true'
-elif [[ "${GPU_NVIDIA}" == 'true' ]]; then
-    export OSS_GPU_DRIVERS='false'
-fi
-grep 'AuthenticAMD' /proc/cmdline && export CPU_VENDOR='AMD'
-grep 'GenuineIntel' /proc/cmdline && export CPU_VENDOR='Intel'
-NETWORKING_HOSTID="$(head -c4 /dev/urandom | od -A none -t x4 | xargs)"
-BOOT_UUID="$(head -c4 /dev/urandom | od -A none -t x4 | xargs)"
-RPIF_UUID="$(head -c4 /dev/urandom | od -A none -t x4 | xargs)"
-NETWORKING_INTERFACE="$(grep -i "eth\|enp\|enabcm\|wl" /proc/net/dev | sort | head -n 1 | awk -F : '{print $1}')"
-TOTAL_MEM_KIB=$(grep 'MemTotal' /proc/meminfo | awk '{print $2}')
-export OS_DRIVE="${1}"
-export MACHINE_HOSTNAME="${2}"
-export PARTITION_LAYOUT="${3}"
+export HOSTNAME
+export TARGET_DRIVE
+export INTERMEDIATE_PART
+export ROOT_PART_SIZE
 export MOUNT_PATH='/mnt'
-export CUSTOM_HOST_CONFIG="${MOUNT_PATH}/etc/nixos/host-specific-configuration.nix"
-export TOTAL_MEM_GIB=$(( TOTAL_MEM_KIB / 1024 / 1024 ))
-export RASP_PART_SIZE='64M'
-export BOOT_PART_SIZE='1G'
-export VARL_PART_SIZE='6G'
-export BATTERY_POWERED_DEVICE
-export NETWORKING_HOSTID
-export BOOT_UUID
-export RPIF_UUID
-export NETWORKING_INTERFACE
-export CAN_TOUCH_EFI_VARS
 
-if [ "${TOTAL_MEM_GIB}" -lt 3 ]; then
-    >&2 echo "$0: You might get an OOM with less than 3GiB of RAM."
-    exit 1
-fi
+################################################################################
+# installation actually starts here
+################################################################################
+set -x
 
 # make sure that $MOUNT_PATH is empty
 # otherwise, bad things happen
 mount | grep " on ${MOUNT_PATH}" && umount --recursive --force "${MOUNT_PATH}"
 
-if [ -b "${OS_DRIVE}" ]; then
-    if echo "${OS_DRIVE}" | grep "sd\|vd"; then
-        export INTERMEDIATE_PART="${OS_DRIVE}"
-    elif echo "${OS_DRIVE}" | grep "mmcblk\|nvme\|loop"; then
-        export INTERMEDIATE_PART="${OS_DRIVE}p"
-    else
-        >&2 echo "$0: unable to decide how to partition '${OS_DRIVE}'"
-        exit 1
-    fi
-else
-    >&2 echo "$0: no such drive exists"
-    exit 1
+# now we partition
+./scripts/partition-disk.sh
+
+# finally, we install NixOS
+TOTAL_MEM_IN_KIB="$(grep 'MemTotal' /proc/meminfo | awk '{print $2}')"
+TOTAL_MEM_IN_GIB="$(( TOTAL_MEM_IN_KIB / 1024 / 1024 ))"
+MIN_MEMORY_IN_GIB='4'
+if [[ "${TOTAL_MEM_IN_GIB}" -lt "${MIN_MEMORY_IN_GIB}" ]]; then
+    echo "WARNING: Total memory is less than ${MIN_MEMORY_IN_GIB} GB. You might get an OOM-kill ... "
 fi
-
-PARTITIONING_SCRIPT="$(pwd)/scripts/standard-partitions.sh"
-if [[ "${PARTITION_LAYOUT}" == 'rpi' ]]; then
-    ROOT_PART_SIZE='80G'
-    PARTITIONING_SCRIPT="$(pwd)/scripts/raspberry-pi-partitions.sh"
-elif [[ "${PARTITION_LAYOUT}" == 'desktop' ]]; then
-    ROOT_PART_SIZE='256G'
-elif [[ "${PARTITION_LAYOUT}" == 'router' ]]; then
-    ROOT_PART_SIZE='32G'
-elif [[ "${PARTITION_LAYOUT}" == 'virt' ]]; then
-    ROOT_PART_SIZE='48G'
-else
-    >&2 echo "$0: invalid argument for 'disk partition layout'"
-    >&2 echo "$0: possible values are 'desktop', 'rpi', 'virt'"
-    exit 1
-fi
-export ROOT_PART_SIZE
-
-# partition, format and mount
-"${PARTITIONING_SCRIPT}"
-
-# prepare installation
-mkdir -p "${MOUNT_PATH}/etc/nixos"
-nixos-generate-config --root "${MOUNT_PATH}"
-if [[ "${PARTITION_LAYOUT}" == 'router' ]]; then
-    cp -v nixos-configuration/router.nix "${MOUNT_PATH}/etc/nixos/configuration.nix"
-else
-    cp -vR nixos-configuration/* "${MOUNT_PATH}/etc/nixos"
-fi
-
-# all host-specific configuration
-# - generate 'networking.hostId' for ZFS
-# - enable Intel/AMD microcode loading if either CPU is detected
-# - adding NVIDIA GPU support if it is detected
-# - enabling KDE if the machine if $PARTITION_LAYOUT is either 'desktop'
-# - enabling BSPWM if the machine if $PARTITION_LAYOUT is either 'virt'
-# - enabling Podman containers if $MACHINE_HOSTNAME is 'reddish' (Raspberry Pi 4 Model B 8GB)
-"$(pwd)/scripts/deviation.sh"
-
-# wait for the user to verify the contents of '${MOUNT_PATH}/etc/nixos'
-set +x
-echo 'Every step except for installation has been performed.'
-echo "Please review ${MOUNT_PATH}/etc/nixos/host-specific-configuration.nix"
-set -x
-read WAIT_UNCONDITIONALLY
-
-# install NixOS
-nixos-install --no-root-password --root "${MOUNT_PATH}" --show-trace
+nixos-install \
+    --root ${MOUNT_PATH} \
+    --show-trace \
+    --no-root-password \
+    --flake ".#${HOSTNAME}"
 
 # very very initial setup for 'pratham'
-cp "$(pwd)/scripts/chroot-as-pratham.sh" "${MOUNT_PATH}/home/pratham"
-nixos-enter --root "${MOUNT_PATH}" -c 'sudo -i -u pratham bash /home/pratham/chroot-as-pratham.sh'
-rm "${MOUNT_PATH}/home/pratham/chroot-as-pratham.sh"
+# shellcheck disable=SC2207
+REAL_USER_LIST=( $(awk -F ':' '{print $6}' "${MOUNT_PATH}/etc/passwd" | grep '/home/' | awk -F '/' '{print $NF}') )
+CHROOT_USER_SCRIPT='chroot-user-setup.sh'
+for NIXOS_USER in "${REAL_USER_LIST[@]}"; do
+    DESTINATION="/home/${NIXOS_USER}/${CHROOT_USER_SCRIPT}"
+    cp "scripts/${CHROOT_USER_SCRIPT}" "${MOUNT_PATH}${DESTINATION}"
+    nixos-enter --root "${MOUNT_PATH}" -c "sudo -i -u ${NIXOS_USER} bash ${DESTINATION}"
+    rm "${MOUNT_PATH}${DESTINATION}"
+done
 
 # done!
-sync; sync; sync; sync;
-sleep 10
 sync; sync; sync; sync;
 umount -R "${MOUNT_PATH}"
