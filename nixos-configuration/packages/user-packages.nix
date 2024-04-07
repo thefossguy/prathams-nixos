ef11b8632facbfb560451764d92cfc945f1431c5
Pratham Patel <prathampatel@thefossguy.com>
2024-04-07 10:54:40 +0530

    minor formatting

--- nixos-configuration/packages/user-packages.nix
+++ nixos-configuration/packages/user-packages.nix
@@ -11,11 +11,11 @@ let
     nativeBuildInputs = prev.nativeBuildInputs or [] ++ (with pkgs; [ makeWrapper ]);
     postInstall = (prev.postInstall or "") + ''
       wrapProgram $out/bin/aerc \
-        --prefix PATH : ${pkgs.lib.makeBinPath packagesInPathForAerc}
+          --prefix PATH : ${pkgs.lib.makeBinPath packagesInPathForAerc}
     '';
   });
 
-  linuxPackages =  lib.optionals (pkgs.stdenv.isLinux) (with pkgs; [
+  linuxPackages = lib.optionals (pkgs.stdenv.isLinux) (with pkgs; [
     cargo-valgrind
     inotify-tools
     rpm
@@ -111,37 +111,37 @@ in
     nix-prefetch-github
   ]);
 
-    programs = {
-      aria2.enable = true;
-      bat.enable = true;
-      bottom.enable = true;
-      broot.enable = true;
-      btop.enable = true;
-      ripgrep.enable = true;
-      tealdeer.enable = true;
-      yt-dlp.enable = true;
-      zoxide.enable = true;
+  programs = {
+    aria2.enable = true;
+    bat.enable = true;
+    bottom.enable = true;
+    broot.enable = true;
+    btop.enable = true;
+    ripgrep.enable = true;
+    tealdeer.enable = true;
+    yt-dlp.enable = true;
+    zoxide.enable = true;
 
-      direnv = {
-        enable = true;
-        enableBashIntegration = true;
-        nix-direnv.enable = true;
-      };
-
-      neovim = {
-        enable = true;
-        extraPackages = with pkgs; [
-          clang-tools # provides clangd
-          gcc # for nvim-tree's parsers
-          lldb # provides lldb-vscode
-          lua-language-server
-          nil # language server for Nix
-          nixpkgs-fmt
-          nodePackages.bash-language-server
-          ruff
-          shellcheck
-          tree-sitter # otherwise nvim complains that the binary 'tree-sitter' is not found
-        ];
-      };
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
     };
+
+    neovim = {
+      enable = true;
+      extraPackages = with pkgs; [
+        clang-tools # provides clangd
+        gcc # for nvim-tree's parsers
+        lldb # provides lldb-vscode
+        lua-language-server
+        nil # language server for Nix
+        nixpkgs-fmt
+        nodePackages.bash-language-server
+        ruff
+        shellcheck
+        tree-sitter # otherwise nvim complains that the binary 'tree-sitter' is not found
+      ];
+    };
+  };
 }
