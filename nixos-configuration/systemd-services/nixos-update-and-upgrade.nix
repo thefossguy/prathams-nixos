527d39ba57aff0af978d6f0ab09c0186552fe700
Pratham Patel <prathampatel@thefossguy.com>
2024-04-07 16:16:03 +0530

    remove temp flake checkout

--- nixos-configuration/systemd-services/nixos-update-and-upgrade.nix
+++ nixos-configuration/systemd-services/nixos-update-and-upgrade.nix
@@ -54,7 +54,6 @@ in
         [[ ! -d ${flakeUri} ]] && git clone https://gitlab.com/thefossguy/prathams-nixos ${flakeUri}
 
         pushd ${flakeUri}
-        [[ ! -f flake.nix ]] && git checkout flake-transition
         git restore flake.lock
         git pull
         nix flake update
