5b83053864dd24d4e5942d296d7ea6fb345581a9
Pratham Patel <prathampatel@thefossguy.com>
2024-05-26 05:32:34 +0530

    reorganize ("flatten") inputs, no semantic difference

--- flake.nix
+++ flake.nix
@@ -3,11 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
