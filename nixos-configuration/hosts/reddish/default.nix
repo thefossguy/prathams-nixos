83eaa30e2e19c9f9d6ae004734a80321fe6bc22c
Pratham Patel <prathampatel@thefossguy.com>
2024-06-15 13:22:35 +0530

    no semantic change

--- nixos-configuration/hosts/reddish/default.nix
+++ nixos-configuration/hosts/reddish/default.nix
@@ -3,6 +3,22 @@
 {
   imports = [ ../_raspberry-pi/4/default.nix ];
 
+  custom-options.enableRootlessContainers = true;
+  networking.firewall.allowedTCPPorts = [
+    8001 # caddy HTTP
+    8002 # caddy HTTPS
+    8003 # personal blog
+    8004 # machine-setup/documentation blog
+    8005 # Gitea web UI
+    8006 # Gitea SSH
+    8008 # Uptime Kuma web UI
+    8009 # Transmission web UI
+    8010 # Transmission torrent comm port (TCP)
+  ];
+  networking.firewall.allowedUDPPorts = [
+    9001 # Transmission torrent comm port (UDP)
+  ];
+
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/15D7-1EF4";
     fsType = "vfat";
@@ -22,20 +38,4 @@
     device = "/dev/disk/by-uuid/cb9dcd35-891b-438f-baa1-fd3278dc3069";
     fsType = "xfs";
   };
-
-  custom-options.enableRootlessContainers = true;
-  networking.firewall.allowedTCPPorts = [
-    8001 # caddy HTTP
-    8002 # caddy HTTPS
-    8003 # personal blog
-    8004 # machine-setup/documentation blog
-    8005 # Gitea web UI
-    8006 # Gitea SSH
-    8008 # Uptime Kuma web UI
-    8009 # Transmission web UI
-    8010 # Transmission torrent comm port (TCP)
-  ];
-  networking.firewall.allowedUDPPorts = [
-    9001 # Transmission torrent comm port (UDP)
-  ];
 }
