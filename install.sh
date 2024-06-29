8d2d9839a9bc0648bfd0d16c0a49f0c31543495e
Pratham Patel <prathampatel@thefossguy.com>
2024-06-29 20:35:07 +0530

    install.sh: fix typo

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env dash
-script -c "sudo ./scripts/installation-scripts/real-installer.sh $1 $2" "installer-log-$2.log"
+script -c "sudo ./scripts/installation-scripts/installer.sh $1 $2" "installer-log-$2.log"
