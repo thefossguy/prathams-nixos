e0784db2e09ea7523444f99bd50073f723eed675
Pratham Patel <prathampatel@thefossguy.com>
2024-07-05 22:33:15 +0530

    chaturvyas: zfs: "init"

--- nixos-configuration/hosts/chaturvyas/default.nix
+++ nixos-configuration/hosts/chaturvyas/default.nix
@@ -1,6 +1,52 @@
-{ ... }:
+{ config, ... }:
+
+let
+  zpoolName = "${config.networking.hostName}-zpool";
+  datasetName = "nix-everything";
+  zfsHelperScriptFromScratch = ''
+    # **SET "LBA FORMAT" FIRST**
+    # 1. Check what LBA Format is being used
+    #sudo nvme id-ns -H </dev/nvme> | grep 'LBA Format'
+    # 2. Find the "best" format
+    #sudo nvme id-ns -H </dev/nvme> | grep 'LBA Format' | grep 'Best'
+    # 2. Use the "best" format
+    #sudo nvme format --lbaf=<the "best" LBA format> --force </dev/nvme>
+
+    # in case you want to import a zpool for destroying
+    #sudo zpool import -N "${zpoolName}"
+
+    # get the options specified in `zpool create -o` with `man 7 zpoolprops`
+    sudo zpool create \
+        -o ashift=12 \
+        -o autotrim=off \
+        -o compatibility=off \
+        -o listsnapshots=on \
+        -O atime=off \
+        -O checksum=fletcher4 \
+        -O compression=zstd-fast \
+        -O primarycache=none \
+        -O relatime=off \
+        -O sync=always \
+        -O xattr=sa \
+        -m none \
+        ${zpoolName} raidz1 nvme0n1 nvme1n1 nvme2n1 nvme3n1
+
+    # get the options specified in `zfs create <option=value>` with `man 7 zfsprops`
+    sudo zfs create \
+        -o mountpoint=/nix \
+        -o recordsize=64K \
+        -u \
+        ${zpoolName}/${datasetName}
+
+    # Or just mount (from installer)
+    sudo zpool export "${zpoolName}"
+    sudo zpool import "${zpoolName}" -R /mnt
+  '';
+in
 
 {
+  imports = [ ../../includes/zfs/default.nix ];
+
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/3A4D-C659";
     fsType = "vfat";
@@ -20,4 +66,12 @@
     device = "/dev/disk/by-uuid/0dc6547c-757c-4dbc-aa54-47f44e9c2598";
     fsType = "xfs";
   };
+
+  fileSystems."/nix" = {
+    device = "${zpoolName}/${datasetName}";
+    fsType = "zfs";
+    options = [ "zfsutil" ];
+  };
+
+  environment.etc."zfsHelperScriptFromScratch".text = zfsHelperScriptFromScratch;
 }
