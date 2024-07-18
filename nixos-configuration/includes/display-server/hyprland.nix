556bb30e195b8ead381382cb60ace5389f20e076
Pratham Patel <prathampatel@thefossguy.com>
2024-07-18 19:18:26 +0530

    kwallet isn't unlocked automatically, for some reason; switch to doing it manually using KWalletManager

--- nixos-configuration/includes/display-server/hyprland.nix
+++ nixos-configuration/includes/display-server/hyprland.nix
@@ -7,11 +7,6 @@
   programs.waybar.enable = true;
   services.hypridle.enable = true;
 
-  security.pam.services.login.kwallet = {
-    enable = true;
-    package = pkgs.kdePackages.kwallet-pam;
-  };
-
   services = {
     displayManager = {
       defaultSession = "hyprland";
@@ -34,6 +29,7 @@
     grim # screenshot utility
     kdePackages.kdeconnect-kde
     kdePackages.kdenlive
+    kdePackages.kwalletmanager
     kdePackages.okular # the universal document viewer (good for previews)
     libnotify # for some reason, this isn't bundled with a notification daemon ('mako' in my case)
     mako # notification daemon
