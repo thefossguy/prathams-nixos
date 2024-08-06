312c86f55a796a95c22f8d0378d0f569d290c1f6
Pratham Patel <prathampatel@thefossguy.com>
2024-08-06 21:02:21 +0530

    add nixos module to enable nix-serve/nix-serve-ng

---
+++ nixos-configuration/includes/local-nix-cahe/default.nix
@@ -0,0 +1,20 @@
+{ config, lib, ... }:
+
+lib.mkIf (config.custom-options.isNixCacheMachine or false) {
+  services.nix-serve = {
+    enable = true;
+    openFirewall = true;
+    port = 5000;
+    secretKeyFile = "/my-nix-binary-cache/cache-priv-key.pem";
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts = {
+      "nixcache.chaturvyas.localhost" = {
+        locations."/".proxyPass = "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
+      };
+    };
+  };
+}
