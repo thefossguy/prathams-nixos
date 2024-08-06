ca80b9892a668124851c8c36c2fa509907d95afa
Pratham Patel <prathampatel@thefossguy.com>
2024-08-06 21:33:21 +0530

    rpi5: minimal support merged in linux-next, remove vendor kernel
    https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/commit/arch/arm64/boot/dts/broadcom/bcm2712-rpi-5-b.dts?id=faa3381267d01a389b13038504079c85c0bc4392

--- nixos-configuration/includes/raspberry-pi/5/default.nix
+++ nixos-configuration/includes/raspberry-pi/5/default.nix
@@ -1,36 +1,10 @@
-{ lib, pkgs, nixpkgs, ... }:
+{ lib, pkgs, ... }:
 
-let
-  linux_rpi5 = pkgs.linux_rpi4.override {
-    rpiVersion = 5;
-
-    argsOverride = rec {
-      version = "${modDirVersion}-vendor-rpi5-16k";
-      modDirVersion = "6.6.35";
-
-      defconfig = "bcm2712_defconfig"; # only difference between this and 'bcm2711_defconfig' is 16K pages
-
-      src = pkgs.fetchFromGitHub {
-        owner = "raspberrypi";
-        repo = "linux";
-        rev = "d2813c02131b9ddf938277f4123da7ccbd113ea7";
-        hash = "sha256-pzjgCWG9FhMU3LCZnvz5N4jYfaaJQDT6Pv5lD/3zsm4=";
-      };
-
-      kernelPatches = [
-        { name = "bridge_stp_helper";  patch = (nixpkgs.outPath + "/pkgs/os-specific/linux/kernel/bridge-stp-helper.patch"); }
-        { name = "request_key_helper"; patch = (nixpkgs.outPath + "/pkgs/os-specific/linux/kernel/request-key-helper.patch"); }
-      ];
-
-      features.efiBootStub = lib.mkForce false;
-    };
-  };
-in {
+{
   imports = [ ../common/default.nix ];
 
   boot = {
     kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
-    #kernelPackages = lib.mkForce (pkgs.linuxPackagesFor linux_rpi5);
     initrd.availableKernelModules = [
       "nvme"
       "usbhid"
