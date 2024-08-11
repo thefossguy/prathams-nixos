f29148deff10980d1333382170060366e909f49f
Pratham Patel <prathampatel@thefossguy.com>
2024-08-11 10:14:21 +0530

    update lockfile -> upgrade nixos -> continuous build

--- nixos-configuration/systemd-services/continuous-build.nix
+++ nixos-configuration/systemd-services/continuous-build.nix
@@ -5,10 +5,8 @@
   systemd = {
     services."continuous-build" = {
       enable = true;
-      after    = [ "update-nixos-flake-inputs.service" ];
-      requires = [ "update-nixos-flake-inputs.service" ];
-      before   = [ "custom-nixos-upgrade.service" ];
-      wantedBy = [ "custom-nixos-upgrade.service" ];
+      after    = [ "custom-nixos-upgrade.service" ];
+      requires = [ "custom-nixos-upgrade.service" ];
       path = [ pkgs.nix ];
 
       serviceConfig = {
