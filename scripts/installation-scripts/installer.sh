73d7898a1eb733f300a5a48cff6a9a8e4cbc2900
Pratham Patel <prathampatel@thefossguy.com>
2024-08-31 23:13:49 +0530

    forgot to replace paths in scripts/ after moving files around

--- scripts/installation-scripts/installer.sh
+++ scripts/installation-scripts/installer.sh
@@ -53,7 +53,7 @@ fi
 # shellcheck disable=SC2086
 nix ${nix_flake_flags} build --dry-run --verbose --trace-verbose --print-build-logs --show-trace .#nixosConfigurations."${HOSTNAME}".config.system.build.toplevel
 # shellcheck disable=SC2086
-if grep 'fsType = "zfs"' "nixos-configuration/hosts/${HOSTNAME}/default.nix"; then
+if grep 'fsType = "zfs"' "nixos-configuration/systems/${HOSTNAME}/default.nix"; then
     ZFS_IN_USE=1
     zpoolName="${HOSTNAME}-zpool"
     export zpoolName
