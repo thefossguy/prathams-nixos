244090c07688c47b96d370ed3a8894aebfd03c5d
Pratham Patel <prathampatel@thefossguy.com>
2024-11-20 11:26:51 +0530

    do not build the uboot update script on the nixos ISO

--- nixos-configuration/modules/soc-support/u-boot-updater.nix
+++ nixos-configuration/modules/soc-support/u-boot-updater.nix
@@ -10,42 +10,37 @@ let
     raajan     = unstablePkgs.ubootRaspberryPi5;
     vaaman     = unstablePkgs.ubootVisionFive2;
     vaayu      = unstablePkgs.ubootVisionFive2;
+
+    # A placeholder for the NixOS ISO. Doesn't do anything because of the `lib.mkIf`.
+    nixos      = pkgs.ubootTools;
   };
   selectedUbootPackage = ubootPackages."${config.networking.hostName}";
-
   ddFlags = "conv=sync";
 
-  isRk3588 = (config.customOptions.socSupport.armSoc == "rk3588");
-  rk3588UbootUpgradeScript = ''
-    if [[ '${isRk3588}' == 'true' ]]; then
-        if [[ -c /dev/mtd0 ]]; then
-            if ! grep --text --null --quiet 'U-Boot SPL ${selectedUbootPackage.version}' /dev/mtd0; then
-                dd ${ddFlags} if=${selectedUbootPackage.outPath}/u-boot-rockchip-spi.bin of=/dev/mtd0
+  rk3588UbootUpgradeScript = if (config.customOptions.socSupport.armSoc == "rk3588") then ''
+    if [[ -c /dev/mtd0 ]]; then
+        dd ${ddFlags} if=${selectedUbootPackage.outPath}/u-boot-rockchip-spi.bin of=/dev/mtd0
+    fi
+
+    EMMC_DEVICES=('/dev/mmcblk0' '/dev/mmcblk1')
+    for MMC_DEV in "''${EMMC_DEVICES[@]}"; do
+        if [[ -b "''${MMC_DEV}" ]]; then
+            if fdisk -l "''${MMC_DEV}" | grep 'EFI System' | grep -q 65536; then
+                dd ${ddFlags} if=${selectedUbootPackage.outPath}/u-boot-rockchip.bin bs=512 seek=64 of="''${MMC_DEV}"
             fi
         fi
+    done
+  '' else "";
 
-        EMMC_DEVICES=('/dev/mmcblk0' '/dev/mmcblk1')
-        for MMC_DEV in "''${EMMC_DEVICES[@]}"; do
-            if [[ -b "''${MMC_DEV}" ]]; then
-                if fdisk -l "''${MMC_DEV}" | grep 'EFI System' | grep -q 65536; then
-                    if ! grep --text --null --quiet 'U-Boot SPL ${selectedUbootPackage.version}' "''${MMC_DEV}"; then
-                        dd ${ddFlags} if=${selectedUbootPackage.outPath}/u-boot-rockchip.bin bs=512 seek=64 of="''${MMC_DEV}"
-                    fi
-                fi
-            fi
-        done
-    fi
-  '';
+  rpiUbootUpgradeScript = if ((config.customOptions.socSupport.armSoc == "rpi4") || (config.customOptions.socSupport.armSoc == "rpi5")) then ''
+    # Will only update if versions differ, so no need to check for version
+    # and worry about flash degradation with write cycles.
+    ${pkgs.rsync}/bin/rsync --quiet --no-motd --checksum --recursive --progress --stats ${pkgs.rpiUbootAndFirmware}/ /boot/
+  '' else "";
+in
 
-  isRaspberryPi = ((config.customOptions.socSupport.armSoc == "rpi4") || (config.customOptions.socSupport.armSoc == "rpi5"));
-  rpiUbootUpgradeScript = ''
-    if [[ '${isRaspberryPi}' == 'true' ]]; then
-        # Will only update if versions differ, so no need to check for version
-        # and worry about flash degradation with write cycles.
-        ${pkgs.rsync}/bin/rsync --quiet --no-motd --checksum --recursive --progress --stats ${pkgs.rpiUbootAndFirmware}/ /boot/
-    fi
-  '';
-in {
+# Do not build this for the ISO
+lib.mkIf ((config.isoImage.isoName or "") != "") {
   boot = {
     kernelParams = [
       "custom_options.uboot_version=${selectedUbootPackage.version}"
