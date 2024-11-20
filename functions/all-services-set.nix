e969c18b22712cd92227da6ee393a2e462622f04
Pratham Patel <prathampatel@thefossguy.com>
2024-11-20 21:58:41 +0530

    improve systemd.time

--- functions/all-services-set.nix
+++ functions/all-services-set.nix
@@ -6,11 +6,12 @@ let
 
   systemdTime = {
     timeZone = "Asia/Kolkata";
-    # systemd.time format: dayOfWeek YYYY-MM-DD hourOfDay:minuteOfHour:secondsOfMinute
-    Hourly = "* *-*-* *:00*00 ${systemdTime.timeZone}";
-    Daily = { hourOfDay ? "00" }: "* *-*-* ${hourOfDay}:00:00 ${systemdTime.timeZone}";
-    Weekly = { dayOfWeek, hourOfDay ? "00" }: "${dayOfWeek} *-*-* ${hourOfDay}:00:00 ${systemdTime.timeZone}";
-    Monthly = { dayOfWeek, dayOfMonth, hourOfDay ? "00" }: "${dayOfWeek} *-*-${dayOfMonth} ${hourOfDay}:00:00 ${systemdTime.timeZone}";
+    # systemd.time format: ${weekday:-} YYYY-MM-DD hour:minute:second
+    # The `weekday` must be [non-]abbreviated and not the catch-all `*`.
+    Hourly = "*-*-* *:00*00 ${systemdTime.timeZone}";
+    Daily = { hour ? "00" }: "*-*-* ${hour}:00:00 ${systemdTime.timeZone}";
+    Weekly = { weekday, hour ? "00" }: "${weekday} *-*-* ${hour}:00:00 ${systemdTime.timeZone}";
+    Monthly = { weekday, day, hour ? "00" }: "${weekday} *-*-${day} ${hour}:00:00 ${systemdTime.timeZone}";
   };
   mkServiceConfig = { unitName, onCalendar ? "", afterUnits ? [], requiredUnits ? [], ... }: {
     inherit unitName onCalendar afterUnits requiredUnits;
@@ -24,7 +25,7 @@ in rec {
     unitName = "custom-nixos-upgrade";
     afterUnits = [ "${updateNixosFlakeInputs.unitName}.service" ];
     requiredUnits = customNixosUpgrade.afterUnits;
-    onCalendar = if isLaptop then systemdTime.Hourly else (systemdTime.Daily { hourOfDay = "05"; });
+    onCalendar = if isLaptop then systemdTime.Hourly else (systemdTime.Daily { hour = "05"; });
   };
 
   continuousBuild = mkServiceConfig {
@@ -42,45 +43,45 @@ in rec {
 
   scheduledReboots = mkServiceConfig {
     unitName = "scheduled-reboots";
-    onCalendar = systemdTime.Weekly { dayOfWeek = "Mon"; hourOfDay = "04"; };
+    onCalendar = systemdTime.Weekly { weekday = "Mon"; hour = "04"; };
     afterUnits = [ "${customNixosUpgrade.unitName}.service" ];
     requiredUnits = scheduledReboots.afterUnits;
   };
 
   zpoolMaintainenceWeekly = mkServiceConfig {
     unitName = "zpool-maintainence-weekly";
-    onCalendar = systemdTime.Weekly { dayOfWeek = "Fri"; };
+    onCalendar = systemdTime.Weekly { weekday = "Fri"; };
   };
 
   zpoolMaintainenceMonthly = mkServiceConfig {
     unitName = "zpool-maintainence-monthly";
-    onCalendar = systemdTime.Monthly { dayOfWeek = "Fri"; dayOfMonth = "01..07"; };
+    onCalendar = systemdTime.Monthly { weekday = "Fri"; day = "01..07"; };
   };
 
   customHomeManagerUpgrade = mkServiceConfig {
     unitName = "custom-home-manager-upgrade";
-    onCalendar = systemdTime.Weekly { dayOfWeek = "Mon"; hourOfDay = "05"; };
+    onCalendar = systemdTime.Weekly { weekday = "Mon"; hour = "05"; };
     afterUnits = [ "${dotfilesPull.unitName}.service" ];
     requiredUnits = customHomeManagerUpgrade.afterUnits;
   };
 
   dotfilesPull = mkServiceConfig {
     unitName = "dotfiles-pull";
-    onCalendar = systemdTime.Daily { hourOfDay = "05"; };
+    onCalendar = systemdTime.Daily { hour = "05"; };
   };
 
   flatpakManage = mkServiceConfig {
     unitName = "flatpak-manage";
-    onCalendar = systemdTime.Daily { hourOfDay = "05"; };
+    onCalendar = systemdTime.Daily { hour = "05"; };
   };
 
   updateRust = mkServiceConfig {
     unitName = "update-rust";
-    onCalendar = systemdTime.Daily { hourOfDay = "05"; };
+    onCalendar = systemdTime.Daily { hour = "05"; };
   };
 
   getRedhatCsafVex = mkServiceConfig {
     unitName = "get-redhat-csaf-vex";
-    onCalendar = systemdTime.Daily { hourOfDay = "05"; };
+    onCalendar = systemdTime.Daily { hour = "05"; };
   };
 }
