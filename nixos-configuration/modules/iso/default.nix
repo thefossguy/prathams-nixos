8e6e48c7719fb167822b73bafa87b243b00b35da
Pratham Patel <prathampatel@thefossguy.com>
2024-11-26 12:59:54 +0530

    remove zramswap from iso

--- nixos-configuration/modules/iso/default.nix
+++ nixos-configuration/modules/iso/default.nix
@@ -11,17 +11,10 @@ in {
   environment.systemPackages = pkgs.callPackage ./packages.nix { inherit pkgs pkgsChannels; };
   users.users."${nixosSystemConfig.coreConfig.systemUser.username}".initialHashedPassword = lib.mkForce nixosSystemConfig.coreConfig.systemUser.hashedPassword;
 
-  zramSwap = {
-    enable = true;
-    algorithm = "zstd";
-    memoryPercent = 50;
-    swapDevices = 2;
-  };
-
-  isoImage.isoName = lib.mkForce "${config.isoImage.isoBaseName}-${config.system.nixos.label}-${config.boot.kernelPackages.kernel.version}-${isoZfsString}${pkgs.stdenv.hostPlatform.system}.iso";
-
   isoImage = {
     squashfsCompression = "zstd -Xcompression-level 22"; # Highest compression ratio.
+    isoName = lib.mkForce "${config.isoImage.isoBaseName}-${config.system.nixos.label}-${config.boot.kernelPackages.kernel.version}-${isoZfsString}${pkgs.stdenv.hostPlatform.system}.iso";
+
     #squashfsCompression = "lz4 -b 32768"; # Lowest time to compress.
   };
 }
