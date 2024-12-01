9abeb88d09a1c8cb15eba9c15253c1a76179aac7
Pratham Patel <prathampatel@thefossguy.com>
2024-12-01 10:32:09 +0530

    enable rockchip pcie modules in rk3588 soc support

--- nixos-configuration/modules/soc-support/rockchip/rk3588.nix
+++ nixos-configuration/modules/soc-support/rockchip/rk3588.nix
@@ -1,4 +1,8 @@
 { config, lib, pkgs, pkgsChannels, nixosSystemConfig, ... }:
 
 lib.mkIf (config.customOptions.socSupport.armSoc == "rk3588") {
+  boot.initrd.availableKernelModules = [
+    "phy-rockchip-pcie"
+    "pcie-rockchip-host"
+  ];
 }
