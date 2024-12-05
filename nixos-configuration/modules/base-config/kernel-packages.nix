a1911e312dd1bc70c2a612929e708394fdeedf80
Pratham Patel <prathampatel@thefossguy.com>
2024-12-05 10:21:25 +0530

    refactor kernel-packages.nix

--- nixos-configuration/modules/base-config/kernel-packages.nix
+++ nixos-configuration/modules/base-config/kernel-packages.nix
@@ -10,11 +10,13 @@ let
     zfs = nixosSystemConfig.kernelConfig.useLongtermKernel;
   };
 
-  enableRustSupport = (nixosSystemConfig.kernelConfig.enableRustSupport && (
+  # Disable ARM64_64K_PAGES pages on LTS kernels because of ZFS.
+  enableArm64kPages = (!nixosSystemConfig.kernelConfig.useLongtermKernel) && pkgs.stdenv.isAarch64;
+  enableRustSupport = nixosSystemConfig.kernelConfig.enableRustSupport && (
     (localStdenv.isx86_64  && lib.versionAtLeast kernelPackages.version "6.7") ||
     (localStdenv.isAarch64 && lib.versionAtLeast kernelPackages.version "6.9") ||
     (localStdenv.isRiscV64 && lib.versionAtLeast kernelPackages.version "6.10")
-  ));
+  );
 in {
   boot = {
     initrd.supportedFilesystems = lib.mkForce supportedFileSystems;
@@ -24,8 +26,7 @@ in {
       argsOverride = {
         features.rust = enableRustSupport;
         structuredExtraConfig = with lib.kernel; {
-          # Disable ARM64_64K_PAGES pages on LTS kernels because of ZFS.
-          #ARM64_64K_PAGES = if ((!nixosSystemConfig.kernelConfig.useLongtermKernel) && pkgs.stdenv.isAarch64) then yes else unset;
+          ARM64_64K_PAGES = if enableArm64kPages then yes else unset;
         };
       };
     }));
