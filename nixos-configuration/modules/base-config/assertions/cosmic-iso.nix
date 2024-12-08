531a2cfd048c70e27b3547fbf15632627fb4fab3
Pratham Patel <prathampatel@thefossguy.com>
2024-12-08 23:40:42 +0530

    improve COSMIC+ISO assertion even further

--- nixos-configuration/modules/base-config/assertions/cosmic-iso.nix
+++ nixos-configuration/modules/base-config/assertions/cosmic-iso.nix
@@ -1,8 +1,8 @@
 { config, lib, pkgs, pkgsChannels, nixosSystemConfig, ... }:
 
-{
+lib.mkIf config.customOptions.isIso {
   assertions = [{
-    assertion = config.customOptions.isIso && config.customOptions.displayServer.guiSession != "cosmic";
+    assertion = config.customOptions.displayServer.guiSession == "cosmic";
     message = ''
       For some reason, COSMIC cannot be enabled on a NixOS ISO because it does not start.
       Remove this once it is no longer the case.
