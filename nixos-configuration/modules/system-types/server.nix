95b254a599628bbf992b4c9b46ec6abb12aac1b9
Pratham Patel <prathampatel@thefossguy.com>
2024-12-13 13:39:20 +0530

    use the `performance` cpufreq governer

--- nixos-configuration/modules/system-types/server.nix
+++ nixos-configuration/modules/system-types/server.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, pkgsChannels, nixosSystemConfig, ... }:
 
 lib.mkIf (config.customOptions.systemType == "server") {
+  boot.kernelParams = [ "cpufreq.default_governor=performance" ];
   systemd.sleep.extraConfig = ''
     AllowHibernation=no
     AllowSuspend=no
