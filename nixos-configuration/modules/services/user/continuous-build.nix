6318205802e0ecba871b4ccdf964bc4e4a668b9b
Pratham Patel <prathampatel@thefossguy.com>
2024-12-21 23:14:19 +0530

    services/user: continuous-build: fix SSH and file iter
    Shell glob was not working the way I anticipated so I had to come up
    with something and also added a way to verify that the SSH
    authentication works, at the very start of the script.

--- nixos-configuration/modules/services/user/continuous-build.nix
+++ nixos-configuration/modules/services/user/continuous-build.nix
@@ -1,9 +1,12 @@
 { config, lib, pkgs, osConfig ? null, pkgsChannels, nixosSystemConfig, ... }:
 
 let
+  localCacheRemote = "chaturvyas";
   serviceConfig = nixosSystemConfig.extraConfig.allServicesSet.continuousBuild;
   appendedPath = import ../../../../functions/append-to-path.nix {
     packages = with pkgs; [
+      coreutils
+      findutils
       git
       nix
       openssh
@@ -30,10 +33,19 @@ in lib.mkIf (osConfig.customOptions.localCaching.buildsNixDerivations or false) 
         ExecStart = "${pkgs.writeShellScript "${serviceConfig.unitName}-execstart.sh" ''
           set -xeuf -o pipefail
 
+          if ! ssh -t ${localCacheRemote} 'cat /proc/sys/kernel/hostname' 1>/dev/null 2>&1; then
+              echo 'SSH key not present on remote (${localCacheRemote}).'
+              exit 1
+          fi
+
           while [[ -f '/etc/nixos/flake-ci-started.log' ]]; do
               sleep 10
           done
-          nix copy --no-check-sigs --to ssh-ng://chaturvyas /etc/nixos/result*
+
+          nixbuildResults=( $(find /etc/nixos -type l | tr '\r\n' ' ') )
+          for targetFile in ''${nixbuildResults}; do
+              nix copy --no-check-sigs --to ssh-ng://${localCacheRemote} "''${targetFile}"
+          done
         ''}";
       };
     };
