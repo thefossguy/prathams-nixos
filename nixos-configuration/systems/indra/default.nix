736f43c03c728186cb9b58ecffb834a4147e6e0f
Pratham Patel <prathampatel@thefossguy.com>
2024-12-31 17:46:07 +0530

    systems/indra: bluetooth is enabled by default

--- nixos-configuration/systems/indra/default.nix
+++ nixos-configuration/systems/indra/default.nix
@@ -5,7 +5,6 @@
 
   boot.blacklistedKernelModules = [ "snd_hda_codec_hdmi" ]; # we no wants sound over HDMI
   boot.extraModprobeConfig = "options kvm_intel nested=1";
-  hardware.bluetooth.enable = true;
 
   customOptions = {
     cpuMicrocodeVendor = "intel";
