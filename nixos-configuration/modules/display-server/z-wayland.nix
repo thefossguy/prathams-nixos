5b8b8c6304b64ebd208450148470bad8f0cd3e66
Pratham Patel <prathampatel@thefossguy.com>
2025-01-08 18:52:18 +0530

    display-server: sddm: use default value for wayland support

--- nixos-configuration/modules/display-server/z-wayland.nix
+++ nixos-configuration/modules/display-server/z-wayland.nix
@@ -6,8 +6,6 @@ lib.mkIf config.customOptions.displayServer.waylandEnabled {
     wayland-utils
     wl-clipboard
   ];
-  # Wayland support is experimental
-  services.displayManager.sddm.wayland.enable = lib.mkDefault false;
   # Enables the Wayland trackpad gestures in Chroimum/Electron
   environment.sessionVariables.NIXOS_OZONE_WL = lib.mkForce "1";
 }
