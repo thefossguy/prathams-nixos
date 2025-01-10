deef8730a8116fdc8f6003c75af66ad423ef7d85
Pratham Patel <prathampatel@thefossguy.com>
2025-01-10 18:22:00 +0530

    scripts/nix-ci: builder: fix logic error/typo

--- scripts/nix-ci/builder.py
+++ scripts/nix-ci/builder.py
@@ -46,7 +46,7 @@ def get_all_supported_systems() -> None:
 
     if '--use-emulation' in sys.argv:
         for nix_system in ci_variables['supported_systems']:
-            if nix_system != native_system:
+            if nix_system == native_system:
                 continue
             emulation_check_command = [ 'nix', 'run', '.#miscPackages.{}.binfmtCheck'.format(nix_system), ]
             print('DEBUG: Checking emulation for `{}`'.format(emulation_check_command))
