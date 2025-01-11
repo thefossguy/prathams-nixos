cbc2dfc7f492c682bbacb60ad7be7603063d08c0
Pratham Patel <prathampatel@thefossguy.com>
2025-01-11 23:32:53 +0530

    soc/rpi4,rpi5: use ttyAMA0 as console

--- nixos-configuration/modules/soc-support/raspberry-pi/common-rpi-config.nix
+++ nixos-configuration/modules/soc-support/raspberry-pi/common-rpi-config.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, pkgsChannels, nixosSystemConfig, ... }:
 
 lib.mkIf ((config.customOptions.socSupport.armSoc == "rpi4") || (config.customOptions.socSupport.armSoc == "rpi5")) {
-  boot.kernelParams = [ "console=ttyS0,115200" ];
+  boot.kernelParams = [ "console=ttyAMA0,115200" ];
 }
