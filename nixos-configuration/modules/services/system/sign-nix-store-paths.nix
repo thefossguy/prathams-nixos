093ef27674ea8c3e0ad9b6a6baaa38c8add16eb5
Pratham Patel <prathampatel@thefossguy.com>
2025-01-20 14:44:44 +0530

    services/system: sign-nix-store-paths: explicitly mention which `system`s to build (link) for

--- nixos-configuration/modules/services/system/sign-nix-store-paths.nix
+++ nixos-configuration/modules/services/system/sign-nix-store-paths.nix
@@ -42,7 +42,7 @@ lib.mkIf config.customOptions.localCaching.servesNixDerivations {
         # only the ones that are in /etc/nixos/result*. Reducing the time taken.
         pushd /etc/nixos || exit 1
         rm -vf result*
-        python3 ./scripts/nix-ci/builder.py --use-emulation --link-only --nixosConfigurations --homeConfigurations --devShells --packages
+        python3 ./scripts/nix-ci/builder.py --link-only --nixosConfigurations --homeConfigurations --devShells --packages --exclusive-nix-system-aarch64-linux --exclusive-nix-system-x86_64-linux
         popd || exit 1
 
         nix store sign --recursive --key-file /my-nix-binary-cache/cache-priv-key.pem $(find /etc/nixos -type l | tr '\r\n' ' ' | xargs realpath)
