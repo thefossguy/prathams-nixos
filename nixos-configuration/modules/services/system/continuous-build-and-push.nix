0a873acb96dff59d14d5306d35db2185c0e6d169
Pratham Patel <prathampatel@thefossguy.com>
2025-01-22 00:20:26 +0530

    services/continuous-build-and-push: re-organize

--- nixos-configuration/modules/services/system/continuous-build-and-push.nix
+++ nixos-configuration/modules/services/system/continuous-build-and-push.nix
@@ -36,13 +36,13 @@ lib.mkIf config.customOptions.localCaching.buildsNixDerivations {
       };
 
       script = ''
+        rm -vf /etc/nixos/result*
+
         pushd /etc/nixos || exit 1
-        rm -vf ./result*
         python3 ./scripts/nix-ci/builder.py --nixosConfigurations --homeConfigurations --devShells --packages
         popd || exit 1
 
-        nix store ping --store 'ssh-ng://pratham@10.0.0.24?ssh-key=/home/pratham/.ssh/ssh'
-        nix copy --no-check-sigs --to 'ssh-ng://pratham@10.0.0.24?ssh-key=/home/pratham/.ssh/ssh' $(find /etc/nixos -type l | tr '\r\n' ' ' | xargs realpath)
+        nix copy --no-check-sigs --refresh --to 'ssh-ng://pratham@10.0.0.24?ssh-key=/home/pratham/.ssh/ssh' $(find /etc/nixos -type l | tr '\r\n' ' ' | xargs realpath)
       '';
     };
   };
