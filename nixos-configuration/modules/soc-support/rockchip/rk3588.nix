d1a3a06987470ebe5a748dc107f660f203c478c5
Pratham Patel <prathampatel@thefossguy.com>
2025-01-30 17:08:22 +0530

    soc/rk3588: add kmod in initrd to help nvme detection on "pawandev"

--- nixos-configuration/modules/soc-support/rockchip/rk3588.nix
+++ nixos-configuration/modules/soc-support/rockchip/rk3588.nix
@@ -8,5 +8,8 @@
 }:
 
 lib.mkIf (config.customOptions.socSupport.armSoc == "rk3588") {
+  boot.initrd.availableKernelModules = [
+    "phy_rockchip_naneng_combphy"
+  ];
   boot.kernelModules = [ "dw_hdmi_qp" ];
 }
