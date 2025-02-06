a91ca6716eed59e10f7577eddd3ee2fd374c0c05
Pratham Patel <prathampatel@thefossguy.com>
2025-02-06 23:14:58 +0530

    nvidia: force the open driver

--- nixos-configuration/modules/gpu/nvidia.nix
+++ nixos-configuration/modules/gpu/nvidia.nix
@@ -15,11 +15,8 @@ lib.mkIf (builtins.elem "nvidia" config.customOptions.gpuSupport) {
   hardware.nvidia = {
     modesetting.enable = true;
     nvidiaSettings = true;
+    open = lib.mkForce true;
     #package = config.boot.kernelPackages.nvidia_x11_vulkan_beta_open;
-
-    # NixOS now defaults to using the open source kernel driver
-    # https://github.com/NixOS/nixpkgs/pull/337289
-    #open = true;
 
     # Hoping it doesn't ever come to this but just in case I am masochist enough
     # to buy a laptop with an Nvidia dGPU, populate these from the output of
