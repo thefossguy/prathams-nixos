ec7a4d459d0241c42ce370db6528c82bb7027143
Pratham Patel <prathampatel@thefossguy.com>
2025-02-09 18:16:27 +0530

    soc/rk3588: `dw_hdmi_qp` is available after 6.13, gate it

--- nixos-configuration/modules/soc-support/rockchip/rk3588.nix
+++ nixos-configuration/modules/soc-support/rockchip/rk3588.nix
@@ -11,5 +11,7 @@ lib.mkIf (config.customOptions.socSupport.armSoc == "rk3588") {
   boot.initrd.availableKernelModules = [
     "phy_rockchip_naneng_combphy"
   ];
-  boot.kernelModules = [ "dw_hdmi_qp" ];
+  boot.kernelModules =
+    lib.optionals (lib.versionAtLeast config.boot.kernelPackages.kernel.version "6.13")
+      [ "dw_hdmi_qp" ];
 }
