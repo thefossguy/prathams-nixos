e392434e12cbc35418bfd70f875ee208fdd0f207
Pratham Patel <prathampatel@thefossguy.com>
2025-02-24 09:10:54 +0530

    partially revert 2baaa56117402a394077a56ce85caeb7c94fb544
    The `boot.initrd.extraFiles.<name>.source` option requires the assigned
    "value" to be a package, so create a package that is just a copy of what
    will be in `/etc/systemd/network/10-use-mac-addr-in-ifnames.link`.

--- nixos-configuration/modules/base-config/ether-dev-names-with-mac-addr.nix
+++ nixos-configuration/modules/base-config/ether-dev-names-with-mac-addr.nix
@@ -8,8 +8,7 @@
 }:
 
 {
-  boot.initrd.extraFiles."etc/systemd/network/10-use-mac-addr-in-ifnames.link".source =
-    config.environment.etc."systemd/network/10-use-mac-addr-in-ifnames.link".source;
+  boot.initrd.extraFiles."etc/systemd/network/10-use-mac-addr-in-ifnames.link".source = pkgs.etherDevNamesWithMacAddr;
 
   systemd.network.links = {
     "10-use-mac-addr-in-ifnames" = {
@@ -24,4 +23,23 @@
       };
     };
   };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      etherDevNamesWithMacAddr = pkgs.stdenvNoCC.mkDerivation {
+        pname = "ether-dev-names-with-mac-addr";
+        version = "v2024.08";
+
+        phases = [ "buildPhase" ];
+        buildPhase = ''
+          set -x
+          mkdir -p $out
+          cp ${
+            config.environment.etc."systemd/network/10-use-mac-addr-in-ifnames.link".source
+          } $out/10-use-mac-addr-in-ifnames.link
+          set +x
+        '';
+      };
+    })
+  ];
 }
