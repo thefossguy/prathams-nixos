ec06c831b14fea7fcdbd5b921e40910cfda07c72
Pratham Patel <prathampatel@thefossguy.com>
2025-02-28 19:28:31 +0530

    wireguard: add activation script only if wireguard interfaces are enabled

--- nixos-configuration/modules/wg-vpn/default.nix
+++ nixos-configuration/modules/wg-vpn/default.nix
@@ -15,20 +15,22 @@
   ];
 
   networking.dhcpcd.runHook = lib.strings.concatStringsSep "\n" config.customOptions.wireguardOptions.routes;
-  system.activationScripts.checkWireguardPrivateKey.text = ''
-    wireguardInterfaces=( ${
-      lib.concatStringsSep " " (map (x: "'${x}'") config.customOptions.wireguardOptions.enabledVPNs)
-    } )
+  system.activationScripts.checkWireguardPrivateKey.text =
+    lib.strings.optionalString (config.customOptions.wireguardOptions.enabledVPNs != [ ])
+      ''
+        wireguardInterfaces=( ${
+          lib.concatStringsSep " " (map (x: "'${x}'") config.customOptions.wireguardOptions.enabledVPNs)
+        } )
 
-    for wgIface in "''${wireguardInterfaces[@]}"; do
-        privateKeyFilePath="${config.customOptions.wireguardOptions.wgPrivateKeyDir}/''${wgIface}.priv"
-        if [[ ! -f "''${privateKeyFilePath}" ]]; then
-            echo "The private key for wireguard interface ''${wgIface} doesn't exist at ''${privateKeyFilePath}." | ${pkgs.systemd}/bin/systemd-cat --identifier wireguard --priority err
-            _localstatus=1
-        fi
+        for wgIface in "''${wireguardInterfaces[@]}"; do
+            privateKeyFilePath="${config.customOptions.wireguardOptions.wgPrivateKeyDir}/''${wgIface}.priv"
+            if [[ ! -f "''${privateKeyFilePath}" ]]; then
+                echo "The private key for wireguard interface ''${wgIface} doesn't exist at ''${privateKeyFilePath}." | ${pkgs.systemd}/bin/systemd-cat --identifier wireguard --priority err
+                _localstatus=1
+            fi
 
-        chown root:root "''${privateKeyFilePath}"
-        chmod 600 "''${privateKeyFilePath}"
-    done
-  '';
+            chown root:root "''${privateKeyFilePath}"
+            chmod 600 "''${privateKeyFilePath}"
+        done
+      '';
 }
