e699e9e1fdeba93cd2e3a09697879b5d873ec7aa
Pratham Patel <prathampatel@thefossguy.com>
2025-03-02 23:11:41 +0530

    virtualisation: switch EFI firmware from OVMF/AVMF to one provided by QEMU

--- nixos-configuration/modules/home-manager/virt-ovmf.nix
+++ nixos-configuration/modules/home-manager/virt-ovmf.nix
@@ -11,24 +11,31 @@ lib.mkIf nixosSystemConfig.coreConfig.isNixOS {
   home.activation = {
     OVMFActivation = lib.hm.dag.entryAfter [ "installPackages" ] ''
       if [[ "$(uname -m)" == 'aarch64' ]]; then
-          EDKII_CODE_NIX="${pkgs.OVMF.fd}/FV/AAVMF_CODE.fd"
-          EDKII_VARS_NIX="${pkgs.OVMF.fd}/FV/AAVMF_VARS.fd"
+          VARS_ARCH='arm'
       elif [[ "$(uname -m)" == 'x86_64' ]]; then
-          EDKII_CODE_NIX="${pkgs.OVMF.fd}/FV/OVMF_CODE.fd"
-          EDKII_VARS_NIX="${pkgs.OVMF.fd}/FV/OVMF_VARS.fd"
+          VARS_ARCH='i386'
       fi
+      CODE_ARCH="$(uname -m)"
+      EDKII_CODE_NIX="${pkgs.qemu_full}/share/qemu/edk2-''${CODE_ARCH}-code.fd"
+      EDKII_CODE_SEC_NIX="${pkgs.qemu_full}/share/qemu/edk2-''${CODE_ARCH}-secure-code.fd"
+      EDKII_VARS_NIX="${pkgs.qemu_full}/share/qemu/edk2-''${VARS_ARCH}-vars.fd"
 
       EDKII_DIR_HOME='/home/${nixosSystemConfig.coreConfig.systemUser.username}/.local/share/edk2'
-      EDKII_CODE_HOME="''${EDKII_DIR_HOME}/EDKII_CODE"
-      EDKII_VARS_HOME="''${EDKII_DIR_HOME}/EDKII_VARS"
 
       if [ -d "''${EDKII_DIR_HOME}" ]; then
           rm -rf "''${EDKII_DIR_HOME}"
       fi
       mkdir -vp "''${EDKII_DIR_HOME}"
 
-      cp "''${EDKII_CODE_NIX}" "''${EDKII_CODE_HOME}"
-      cp "''${EDKII_VARS_NIX}" "''${EDKII_VARS_HOME}"
+      cp -v "''${EDKII_CODE_NIX}" "''${EDKII_DIR_HOME}/EDK2_CODE"
+      cp -v "''${EDKII_CODE_SEC_NIX}" "''${EDKII_DIR_HOME}/EDK2_CODE_SECURE"
+      cp -v "''${EDKII_VARS_NIX}" "''${EDKII_DIR_HOME}/EDK2_VARS"
+
+      ln -s "''${EDKII_CODE_NIX}" "''${EDKII_DIR_HOME}/edk2_code"
+      ln -s "''${EDKII_CODE_SEC_NIX}" "''${EDKII_DIR_HOME}/edk2_code_secure"
+      ln -s "''${EDKII_VARS_NIX}" "''${EDKII_DIR_HOME}/edk2_vars"
+
+      chmod 644 -v "''${EDKII_DIR_HOME}/EDK2_"*
 
       chown "''${LOGNAME}":"''${LOGNAME}" "''${EDKII_CODE_HOME}" "''${EDKII_VARS_HOME}"
       chmod 644 "''${EDKII_CODE_HOME}" "''${EDKII_VARS_HOME}"
