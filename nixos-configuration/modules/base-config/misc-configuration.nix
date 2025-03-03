db9b5728804d0ae607270502a9f8210cf9ff2986
Pratham Patel <prathampatel@thefossguy.com>
2025-03-03 23:32:08 +0530

    enable kernel samepage merging at every 20 minutes

--- nixos-configuration/modules/base-config/misc-configuration.nix
+++ nixos-configuration/modules/base-config/misc-configuration.nix
@@ -14,6 +14,11 @@ let
   isIso = config.customOptions.isIso;
 in
 {
+  hardware.ksm = {
+    enable = true;
+    sleep = 1200000;
+  };
+
   time = {
     timeZone = "Asia/Kolkata";
     hardwareClockInLocalTime = false;
