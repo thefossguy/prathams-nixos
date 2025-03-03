817f88e7bcfe9ab6f16d7e69556f65d2c5ff6d61
Pratham Patel <prathampatel@thefossguy.com>
2025-03-03 12:26:53 +0530

    services/manually-autostart-libvirt-vms: update VM firmware path before starting them

--- nixos-configuration/modules/services/user/manually-autostart-libvirt-vms.nix
+++ nixos-configuration/modules/services/user/manually-autostart-libvirt-vms.nix
@@ -10,11 +10,14 @@
 
 let
   serviceConfig = nixosSystemConfig.extraConfig.allServicesSet.manuallyAutostartLibvirtVms;
+  editorVar = "/home/${nixosSystemConfig.coreConfig.systemUser.username}/.local/scripts/update-vm-firmware-paths.sh";
   appendedPath = import ../../../../functions/append-to-path.nix {
     packages = with pkgs; [
       bash
       coreutils
       findutils
+      gawk
+      jq
       libvirt
     ];
   };
@@ -39,6 +42,23 @@ lib.mkIf (osConfig.customOptions.virtualisation.enable or false) {
 
       ExecStart = "${pkgs.writeShellScript "${serviceConfig.unitName}-execstart.sh" ''
         set -xeuf -o pipefail
+
+        BUNDLED_QEMU_FIRMWARE_PATH="$(jq --raw-output '.mapping.executable.filename' /var/lib/qemu/firmware/60-edk2-"$(uname -m)".json | awk -F '/' '{print $4}')"
+
+        cat << EOF > ${editorVar}
+        set -xeuf -o pipefail
+        sed -i 's@/nix/store/.*-qemu-.*/share/qemu/@/nix/store/''${BUNDLED_QEMU_FIRMWARE_PATH}/share/qemu/@g' \$1
+        EOF
+
+        echo '--------------------------------------------------------------------------------'
+        cat ${editorVar}
+        echo '--------------------------------------------------------------------------------'
+
+        chmod +x ${editorVar}
+        EDITOR=${editorVar}
+        export EDITOR
+
+        virsh --connect qemu:///session list --all --name | xargs --no-run-if-empty --max-args 1 virsh --connect qemu:///session edit
 
         # Manually append `/run/wrappers/bin` to PATH for `qemu-bridge-helper`
         export PATH=$PATH:/run/wrappers/bin
