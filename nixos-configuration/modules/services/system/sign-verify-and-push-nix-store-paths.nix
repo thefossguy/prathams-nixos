beb511ac8150a606ad37fb583fba87ac2f3d3a30
Pratham Patel <prathampatel@thefossguy.com>
2025-03-05 12:17:21 +0530

    services/sign-verify-and-push-nix-store-paths: use basename for flake.lock's shasum

--- nixos-configuration/modules/services/system/sign-verify-and-push-nix-store-paths.nix
+++ nixos-configuration/modules/services/system/sign-verify-and-push-nix-store-paths.nix
@@ -30,6 +30,7 @@ lib.mkIf config.customOptions.localCaching.servesNixDerivations {
         findutils
         gawk
         git
+        gnused
         nix
         python3
       ];
@@ -66,7 +67,7 @@ lib.mkIf config.customOptions.localCaching.servesNixDerivations {
         for nixIndvHash in "''${nixHashes[@]}"; do
             echo "''${nixIndvHash}" | aws s3 cp - "s3://thefossguy-nix-cache-001-8c0d989b-44cf-4977-9446-1bf1602f0088/''${nixIndvHash}.narinfo"
         done
-        sha512sum /etc/nixos/flake.lock | aws s3 cp - s3://thefossguy-nix-cache-001-8c0d989b-44cf-4977-9446-1bf1602f0088/zeLock
+        sha512sum /etc/nixos/flake.lock | sed -e 's@/etc/nixos/flake.lock@flake.lock@g' | aws s3 cp - s3://thefossguy-nix-cache-001-8c0d989b-44cf-4977-9446-1bf1602f0088/zeLock
         git -C /etc/nixos rev-parse HEAD | aws s3 cp - s3://thefossguy-nix-cache-001-8c0d989b-44cf-4977-9446-1bf1602f0088/zeHead
       '';
     };
