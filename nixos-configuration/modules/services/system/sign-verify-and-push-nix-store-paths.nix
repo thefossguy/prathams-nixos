510786bf7bcdd03301c7c251f012fa5766e35f96
Pratham Patel <prathampatel@thefossguy.com>
2025-03-12 21:53:44 +0530

    services/sign-verify-and-push-nix-store-paths: _sigh_ copy the lockfile instead of the hash, don't be an idiot

--- nixos-configuration/modules/services/system/sign-verify-and-push-nix-store-paths.nix
+++ nixos-configuration/modules/services/system/sign-verify-and-push-nix-store-paths.nix
@@ -67,7 +67,7 @@ lib.mkIf config.customOptions.localCaching.servesNixDerivations {
         for nixIndvHash in "''${nixHashes[@]}"; do
             echo "''${nixIndvHash}" | aws s3 cp - "s3://thefossguy-nix-cache-001-8c0d989b-44cf-4977-9446-1bf1602f0088/''${nixIndvHash}.narinfo"
         done
-        sha512sum /etc/nixos/flake.lock | sed -e 's@/etc/nixos/flake.lock@flake.lock@g' | aws s3 cp - s3://thefossguy-nix-cache-001-8c0d989b-44cf-4977-9446-1bf1602f0088/zeLock
+        aws s3 cp /etc/nixos/flake.lock s3://thefossguy-nix-cache-001-8c0d989b-44cf-4977-9446-1bf1602f0088/flake.lock
         git -C /etc/nixos rev-parse HEAD | aws s3 cp - s3://thefossguy-nix-cache-001-8c0d989b-44cf-4977-9446-1bf1602f0088/zeHead
       '';
     };
