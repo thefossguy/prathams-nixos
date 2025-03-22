41b6e806baa3b5fcf1b81550e555d042a3c93eac
Pratham Patel <prathampatel@thefossguy.com>
2025-03-22 19:46:02 +0530

    enable coolercontrol on laptops

--- nixos-configuration/modules/system-types/laptop.nix
+++ nixos-configuration/modules/system-types/laptop.nix
@@ -13,6 +13,11 @@ lib.mkIf (config.customOptions.systemType == "laptop") {
     "pm_debug_messages"
   ];
 
+  programs.coolercontrol = {
+    enable = true;
+    nvidiaSupport = (builtins.elem "nvidia" config.customOptions.gpuSupport);
+  };
+
   systemd.sleep.extraConfig = ''
     AllowHibernation=yes
     AllowHybridSleep=yes
