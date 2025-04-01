025640119525b5e59ceb2916dcee7226267a7fcc
Pratham Patel <prathampatel@thefossguy.com>
2025-04-01 08:35:07 +0530

    enable the cosmic modules from nixpkgs

--- nixos-configuration/modules/display-server/cosmic.nix
+++ nixos-configuration/modules/display-server/cosmic.nix
@@ -7,10 +7,21 @@
   ...
 }:
 
-lib.mkIf (config.customOptions.displayServer.guiSession == "cosmic") {
-  #services.desktopManager.cosmic.enable = true;
-  #services.displayManager.cosmic-greeter.enable = true;
+lib.mkIf
+  (
+    # The module was added before the release of 25.05
+    (lib.versionAtLeast (lib.versions.majorMinor lib.version) "25.05")
+    && (config.customOptions.displayServer.guiSession == "cosmic")
+  )
+  {
+    services = {
+      displayManager.cosmic-greeter.enable = true;
+      desktopManager.cosmic = {
+        enable = true;
+        xwayland.enable = true;
+      };
+    };
 
-  environment.systemPackages = with pkgs; [
-  ];
-}
+    environment.systemPackages = with pkgs; [
+    ];
+  }
