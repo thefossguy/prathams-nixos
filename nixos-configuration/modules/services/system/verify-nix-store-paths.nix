91bf8e5a6b9891c3298ec607d8fac50564d440a7
Pratham Patel <prathampatel@thefossguy.com>
2025-04-11 20:55:04 +0530

    services/verify-nix-store-paths: fix return code checking on local builders

--- nixos-configuration/modules/services/system/verify-nix-store-paths.nix
+++ nixos-configuration/modules/services/system/verify-nix-store-paths.nix
@@ -31,7 +31,28 @@ in
         Type = "oneshot";
       };
 
-      script = "nix store verify --all --recursive --sigs-needed 1 >/dev/null 2>&1";
+      script = ''
+        set -x
+
+        verificationCode=0
+        isBuilder=${if (config.customOptions.localCaching.buildsNixDerivations or false) then "1" else "0"}
+
+        nix store verify --all --recursive --sigs-needed 1 >/dev/null 2>&1 || verificationCode=$?
+
+        if [[ "''${verificationCode}" -eq 2 ]]; then
+            if [[ "''${isBuilder}" -eq 1 ]]; then
+                # Exit cleanly only if the `nix store verify` command exits
+                # with return code 2 on a builder.
+                exit 0
+            else
+                # Exit with the original return code
+                exit "''${verificationCode}"
+            fi
+        else
+            # Exit with the original return code
+            exit "''${verificationCode}"
+        fi
+      '';
     };
   };
 }
