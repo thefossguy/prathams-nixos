bcb66f9bad652a9baa82d294d4cd0b400bde6f62
Pratham Patel <prathampatel@thefossguy.com>
2025-04-12 09:35:32 +0530

    Revert "services/verify-nix-store-paths: enable only on non-builders and non-cachers"
    This reverts commit f1c8b036a9c551d0afd5f1d36b6e205606af5c0f.

--- nixos-configuration/modules/services/system/verify-nix-store-paths.nix
+++ nixos-configuration/modules/services/system/verify-nix-store-paths.nix
@@ -10,31 +10,49 @@
 let
   serviceConfig = nixosSystemConfig.extraConfig.allServicesSet.verifyNixStorePaths;
 in
-lib.mkIf
-  ((!config.customOptions.localCaching.servesNixDerivations) || (!config.customOptions.localCaching.buildsNixDerivations))
-  {
-
-    systemd = {
-      timers."${serviceConfig.unitName}" = {
-        enable = true;
-        requiredBy = [ "timers.target" ];
-        timerConfig.OnCalendar = serviceConfig.onCalendar;
-        timerConfig.Unit = "${serviceConfig.unitName}.service";
-      };
-
-      services."${serviceConfig.unitName}" = {
-        enable = true;
-        before = serviceConfig.beforeUnits;
-        requiredBy = serviceConfig.requiredByUnits;
-
-        path = [ pkgs.nix ];
-
-        serviceConfig = {
-          User = "root";
-          Type = "oneshot";
-        };
-
-        script = "nix store verify --all --recursive --sigs-needed 1 >/dev/null 2>&1";
-      };
+{
+  systemd = {
+    timers."${serviceConfig.unitName}" = {
+      enable = true;
+      requiredBy = [ "timers.target" ];
+      timerConfig.OnCalendar = serviceConfig.onCalendar;
+      timerConfig.Unit = "${serviceConfig.unitName}.service";
     };
-  }
+
+    services."${serviceConfig.unitName}" = {
+      enable = true;
+      before = serviceConfig.beforeUnits;
+      requiredBy = serviceConfig.requiredByUnits;
+
+      path = [ pkgs.nix ];
+
+      serviceConfig = {
+        User = "root";
+        Type = "oneshot";
+      };
+
+      script = ''
+        set -x
+
+        verificationCode=0
+        isBuilder=${if (config.customOptions.localCaching.buildsNixDerivations or false) then "1" else "0"}
+
+        nix store verify --all --recursive --sigs-needed 1 >/dev/null 2>&1 || verificationCode=$?
+
+        if [[ "''${verificationCode}" -eq 2 ]]; then
+            if [[ "''${isBuilder}" -eq 1 ]]; then
+                # Exit cleanly only if the `nix store verify` command exits
+                # with return code 2 on a builder.
+                exit 0
+            else
+                # Exit with the original return code
+                exit "''${verificationCode}"
+            fi
+        else
+            # Exit with the original return code
+            exit "''${verificationCode}"
+        fi
+      '';
+    };
+  };
+}
