5564bcba0e5658f8be01d9fdddf76f8f1f407e71
Pratham Patel <prathampatel@thefossguy.com>
2025-04-25 17:33:01 +0530

    firefox: add more preferences

--- nixos-configuration/modules/display-server/base-config.nix
+++ nixos-configuration/modules/display-server/base-config.nix
@@ -42,15 +42,23 @@ lib.mkIf (config.customOptions.displayServer.guiSession != "unset") {
       preferencesStatus = "user";
       preferences = {
         "accessibility.force_disabled" = 1;
+        "apz.overscroll.enabled" = false;
+        "browser.cache.max_shutdown_io_lag" = 32;
+        "browser.ctrlTab.sortByRecentlyUsed" = false;
         "browser.discovery.enabled" = false;
         "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
         "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
+        "browser.search.separatePrivateDefault.ui.enabled" = true;
         "browser.tabs.insertAfterCurrent" =  true;
         "browser.tabs.insertAfterCurrentExceptPinned" =  true;
         "browser.tabs.insertRelatedAfterCurrent" = false; # Open at the far right instead of next to current tab
+        "browser.tabs.tabClipWidth" = 999; # Only the active tab has the close button
         "browser.tabs.unloadOnLowMemory" = true;
         "browser.tabs.warnOnClose" = true;
         "browser.translations.enable" = false;
+        "browser.uidensity" = 1;
+        "browser.urlbar.maxRichResults" = 10;
+        "browser.urlbar.suggest.calculator" = 1;
         "browser.warnOnQuit" = true;
         "dnsCacheEntries" = 2000;
         "extensions.getAddons.discovery.api_url" = "";
@@ -58,15 +66,45 @@ lib.mkIf (config.customOptions.displayServer.guiSession != "unset") {
         "extensions.htmlaboutaddons.discover.enabled" = false;
         "extensions.htmlaboutaddons.recommendations.enabled" = false;
         "extensions.pocket.enabled" = false;
+        "extensions.screenshots.disabled" = false;
         "extensions.webservice.discoverURL" = false;
+        "findbar.highlightAll" = true;
+        "identity.fxaccounts.enabled" = true;
         "image.mem.decode_bytes_at_a_time" = 131072;
+        "layout.word_select.eat_space_to_next_word" = false;
+        "media.autoplay.blocking_policy" = 2;
         "media.block-autoplay-until-in-foreground" = true;
         "media.block-play-until-document-interaction" = true;
         "media.block-play-until-visible" = true;
         "middlemouse.paste" = false;
+        "narrate.enabled" = false;
         "network.dnsCacheExpiration" = 7200;
         "network.dnsCacheExpirationGracePeriod" = 3600;
         "network.ssl_tokens_cache_capacity" = 32768;
+        "privacy.resistFingerprinting" = true;
+        "reader.parse-on-load.enabled" = true;
+        "services.sync.prefs.sync.extensions.activeThemeID" = true;
+        "ui.systemUsesDarkTheme" = true;
+        "widget.non-native-theme.scrollbar.style" = 1;
+        "widget.windows.overlay-scrollbars.enabled" = true;
+
+        # Disable checking if Firefox is the default browser or not
+        "browser.shell.checkDefaultBrowser" = false;
+        "browser.shell.defaultBrowserCheckCount" = 1;
+        "browser.shell.didSkipDefaultBrowserCheckOnFirstRun" = true;
+
+        # Smooth scrolling
+        "apz.frame_delay.enabled" = false;
+        "general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS" = 250;
+        "general.smoothScroll.msdPhysics.enabled" = true;
+        "general.smoothScroll.msdPhysics.motionBeginSpringConstant" = 400;
+        "general.smoothScroll.msdPhysics.regularSpringConstant" = 400;
+        "general.smoothScroll.msdPhysics.slowdownMinDeltaMS" = 120;
+        "general.smoothScroll.msdPhysics.slowdownMinDeltaRatio" =  0.4;
+        "general.smoothScroll.msdPhysics.slowdownSpringConstant" = 5000;
+        "mousewheel.min_line_scroll_amount" = 22;
+        "toolkit.scrollbox.horizontalScrollDistance" = 4;
+        "toolkit.scrollbox.verticalScrollDistance" = 5;
 
         # Removes the "blink" when going fullscreen and removes the unnecessary warning
         # https://old.reddit.com/r/firefox/comments/17hlkhp/what_are_your_must_have_changes_in_aboutconfig/k6o9xiv/
@@ -78,6 +116,7 @@ lib.mkIf (config.customOptions.displayServer.guiSession != "unset") {
 
       # https://mozilla.github.io/policy-templates/
       policies = {
+        DisableAppUpdate = true; # Get updates from Nixpkgs instead
         AutofillAddressEnabled = false;
         AutofillCreditCardEnabled = false;
         DefaultDownloadDirectory = "${config.customOptions.userHomeDir}/Downloads";
