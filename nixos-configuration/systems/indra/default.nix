a01e8d86e3da1c702aff793b2f8b8cf7ff1f8804
Pratham Patel <prathampatel@thefossguy.com>
2025-04-29 16:01:53 +0530

    systems/indra: enable kwallet

--- nixos-configuration/systems/indra/default.nix
+++ nixos-configuration/systems/indra/default.nix
@@ -13,6 +13,10 @@
   boot.blacklistedKernelModules = [ "snd_hda_codec_hdmi" ]; # we no wants sound over HDMI
   boot.extraModprobeConfig = "options kvm_intel nested=1";
 
+  # TODO: Remove this on a fresh install
+  # Enable kwallet because that was used in my last setup with hyprland.
+  security.pam.services.login.kwallet.enable = true;
+
   customOptions = {
     displayServer.guiSession = "cosmic";
     gpuSupport = [ "intel" ];
