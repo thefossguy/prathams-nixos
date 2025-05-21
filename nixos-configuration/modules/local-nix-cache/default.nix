8a8962470355ec11ba2962b86a9ec80ce7b6e053
Pratham Patel <prathampatel@thefossguy.com>
2025-05-21 15:27:36 +0530

    fix conditional for accessing the local cache

--- nixos-configuration/modules/local-nix-cache/default.nix
+++ nixos-configuration/modules/local-nix-cache/default.nix
@@ -14,7 +14,9 @@
   ];
 
   nix.settings = {
-    extra-substituters = lib.optionals nixosSystemConfig.extraConfig.canAccessMyNixCache [ "http://10.0.0.24" ];
+    extra-substituters = lib.optionals (!config.customOptions.localCaching.servesNixDerivations) [
+      "${lib.strings.optionalString nixosSystemConfig.extraConfig.canAccessMyNixCache "http://10.0.0.24"}"
+    ];
     extra-trusted-public-keys = [ "10.0.0.24:g29fjBRU/VGj6kkIQqjm0o5sxWduZ1hNNLTnSeF/AAU=" ];
   };
 }
