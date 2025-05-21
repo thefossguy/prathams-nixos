7973e3f4da48c8f9f04a4d92d0f570f94be71b3a
Pratham Patel <prathampatel@thefossguy.com>
2025-05-21 19:24:05 +0530

    services/sign-verify-and-push-nix-store-paths.nix: accommodate for `hans`

--- nixos-configuration/modules/services/system/sign-verify-and-push-nix-store-paths.nix
+++ nixos-configuration/modules/services/system/sign-verify-and-push-nix-store-paths.nix
@@ -51,13 +51,15 @@ lib.mkIf config.customOptions.localCaching.servesNixDerivations {
         # like building the expressions, even for other `system`s, without
         # actually building it. So instead of signing every store path, sign
         # only the ones that are in /etc/nixos/result*. Reducing the time taken.
-        pushd /etc/nixos || exit 1
+        pushd /etc/nixos
         python3 ./scripts/nix-ci/builder.py \
             --nixosConfigurations --homeConfigurations --devShells --packages \
             --exclusive-nix-system-aarch64-linux --exclusive-nix-system-x86_64-linux \
             --evaluate-outPaths --link-outPaths
-        popd || exit 1
+        popd
+      '';
 
+      postStart = lib.strings.optionalString ''
         nixResults=( $(find /etc/nixos -iname 'result*' -type l | tr '\r\n' ' ' | xargs --no-run-if-empty realpath) )
         nixHashes=( $(echo "''${nixResults[@]}" | xargs --no-run-if-empty --max-args 1 basename | awk -F '-' '{print $1}') )
 
@@ -65,9 +67,7 @@ lib.mkIf config.customOptions.localCaching.servesNixDerivations {
         nix store verify --recursive --sigs-needed 1 "''${nixResults[@]}" >/dev/null 2>&1 || \
              nix store repair "''${nixResults[@]}"
 
-        ${lib.strings.optionalString (config.networking.hostName == "chaturvyas") ''
-          nix copy --refresh --to 'ssh-ng://pratham@138.199.146.78?ssh-key=${config.customOptions.userHomeDir}/.ssh/ssh' $(find /etc/nixos -iname 'result*' -type l | tr '\r\n' ' ' | xargs --no-run-if-empty realpath)
-        ''}
+        nix copy --refresh --to 'ssh-ng://pratham@138.199.146.78?ssh-key=${config.customOptions.userHomeDir}/.ssh/ssh' "''${nixResults[@]}"
         aws s3 cp /etc/nixos/flake.lock s3://thefossguy-nix-cache-001-8c0d989b-44cf-4977-9446-1bf1602f0088/flake.lock
       '';
     };
