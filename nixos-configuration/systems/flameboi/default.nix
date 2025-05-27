04ea73a9badde5ff922c82917fe84160321b4ea1
Pratham Patel <prathampatel@thefossguy.com>
2025-05-27 12:29:48 +0530

    systems/flameboi: remove cosmic

--- nixos-configuration/systems/flameboi/default.nix
+++ nixos-configuration/systems/flameboi/default.nix
@@ -11,14 +11,10 @@
   imports = [ ./hardware-configuration.nix ];
 
   customOptions = {
-    displayServer.guiSession = "cosmic";
     gpuSupport = [ "nvidia" ];
     localCaching.buildsNixDerivations = true;
     useMinimalConfig = lib.mkForce false;
     virtualisation.enable = true;
     x86CpuVendor = "amd";
   };
-
-  # Enabling XWayland on a desktop that doesn't sleep is okay.
-  services.desktopManager.cosmic.xwayland.enable = lib.mkForce true;
 }
