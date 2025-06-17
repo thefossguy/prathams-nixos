7f40b0224c1eb01ce0d30c5fe697fc3a65d63c7a
Pratham Patel <prathampatel@thefossguy.com>
2025-06-17 19:16:30 +0530

    systems/mahadev: disable bluetooth (HW not present)

--- nixos-configuration/systems/mahadev/default.nix
+++ nixos-configuration/systems/mahadev/default.nix
@@ -10,6 +10,8 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
+  hardware.bluetooth.enable = lib.mkForce false;
+
   customOptions = {
     displayServer.guiSession = "cosmic";
     socSupport.armSoc = "rk3588";
