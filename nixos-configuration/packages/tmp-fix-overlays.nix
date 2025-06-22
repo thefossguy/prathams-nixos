f35c8e8f43ac7139a862d6b16574c68b186849d7
Pratham Patel <prathampatel@thefossguy.com>
2025-06-22 10:28:56 +0530

    tmp-overlay: add overlay for `landrun`

--- nixos-configuration/packages/tmp-fix-overlays.nix
+++ nixos-configuration/packages/tmp-fix-overlays.nix
@@ -17,6 +17,14 @@ in
   # `overlay.nix` file.
   nixpkgs.overlays = [
     (final: prev: {
+      landrun = prev.landrun.overrideAttrs (oldAttrs: {
+        patches = (prev.patches or [ ]) ++ [
+          (pkgs.fetchpatch {
+            url = "https://github.com/Zouuup/landrun/commit/b7100d0a3adb86c0374f161f862e3067efacc790.patch";
+            hash = "sha256-umFaSjfxfr82ORVGD7DmsLbziJq8zdMp4q199yySWWI=";
+          })
+        ];
+      });
     })
   ];
 }
