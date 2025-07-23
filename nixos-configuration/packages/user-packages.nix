thefossguy/prathams-nixos

commit ffd63829a57bc34a99092740ebaf4462a9fba0b8

no semantic change

nixos-configuration/packages/user-packages.nix
@@ -13,7 +13,7 @@ let
   enableHomelabServices = (osConfig.customOptions.podmanContainers.enableHomelabServices or false);
 
   devPackages = {
-    generic = lib.optionals (!useMinimalConfig) (
+    kernel = lib.optionals (!useMinimalConfig && config.customOptions.kernelDevelopment.enable) (
       with pkgs;
       [
         b4
@@ -200,7 +200,7 @@ in
     ++ darwinPackages
     ++ commonPackagesMinimal
     ++ commonPackages
-    ++ devPackages.generic
+    ++ devPackages.kernel
     ++ devPackages.rust
     ++ packageSets.misc
     ++ packageSets.mozilla

commit 160df92c18e595f54e73b5fec50a4add41c36d41

fix config logic error introduced in ffd63829a57bc34a99092740ebaf4462a9fba0b8

nixos-configuration/packages/user-packages.nix
@@ -13,7 +13,7 @@ let
   enableHomelabServices = (osConfig.customOptions.podmanContainers.enableHomelabServices or false);
 
   devPackages = {
-    kernel = lib.optionals (!useMinimalConfig && config.customOptions.kernelDevelopment.enable) (
+    kernel = lib.optionals (!useMinimalConfig && (config.customOptions.kernelDevelopment.enable or false)) (
       with pkgs;
       [
         b4