1fca442f3ad6a75f6e0f82b2af3269ae42d5880e
Pratham Patel <prathampatel@thefossguy.com>
2025-09-07 13:14:50 +0530

    functions/full-user-set: add user: thefossguy

--- functions/full-user-set.nix
+++ functions/full-user-set.nix
@@ -6,6 +6,13 @@
     isRealUser = true;
   };
 
+  # The `ppatel` user is on the nix-community builders
+  # So no attributes other than `username` and `isRealUser` need to be defined.
+  thefossguy = {
+    username = "thefossguy";
+    isRealUser = true;
+  };
+
   # The `ppatel` user is on my work MBP which is an `aarch64-darwin` machine.
   # So no attributes other than `username` and `isRealUser` need to be defined.
   ppatel = {
