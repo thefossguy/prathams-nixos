79d84f9f0dbf0d4f848b4a03ea9a886cf0510466
Pratham Patel <prathampatel@thefossguy.com>
2025-09-28 21:58:17 +0530

    services/custom-nixos-upgrade: build nixos generation only if cached on non-builders

--- nixos-configuration/modules/services/system/custom-nixos-upgrade.nix
+++ nixos-configuration/modules/services/system/custom-nixos-upgrade.nix
@@ -31,6 +31,8 @@ in
         nix
         nixos-rebuild
         systemd
+        curl
+        gnugrep
       ];
 
       serviceConfig = {
@@ -38,7 +40,34 @@ in
         Type = "oneshot";
       };
 
-      script = "nixos-rebuild boot --show-trace --print-build-logs --flake /etc/nixos#${config.networking.hostName}";
+      script = ''
+        set -xeuf -o pipefail
+
+        ${lib.strings.optionalString (!config.customOptions.localCaching.buildsNixDerivations) ''
+          nixosToplevelOutPath="$(nix eval --raw 2>/dev/null /etc/nixos#nixosConfigurations.${config.networking.hostName}.config.system.build.toplevel || echo 'eval-failed')"
+          if [[ "''${nixosToplevelOutPath}" == 'eval-failed' ]]; then
+              echo 'Could not determine the outPath for your NixOS toplevel build derivation'
+              exit 1
+          fi
+
+          nixosToplevelOutPathHash="$(echo "''${nixosToplevelOutPath}" | cut -c 12-43 || echo 'shouldnt-really-fail')"
+          if [[ "''${nixosToplevelOutPathHash}" == 'shouldnt-really-fail' ]]; then
+              echo 'Could not determine the hash for the outPath for your NixOS toplevel build derivation'
+              exit 1
+          fi
+
+          nixosToplevelIsCached="$(curl "https://nix-cache.thefossguy.com/''${nixosToplevelOutPathHash}.narinfo" 2>/dev/null || echo 'curl-failed')"
+          if [[ "''${nixosToplevelIsCached}" == 'curl-failed' ]]; then
+              echo 'Could not check if the NixOS toplevel derivation is cached or not'
+              exit 1
+          elif ! echo "''${nixosToplevelIsCached}" | grep -q "''${nixosToplevelOutPathHash}"; then
+              echo 'The NixOS toplevel derivation is not cached, yet'
+              exit 1
+          fi
+        ''}
+
+        nixos-rebuild boot --show-trace --print-build-logs --flake /etc/nixos#${config.networking.hostName}
+      '';
     };
   };
 }
