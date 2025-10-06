c6086f07a93351e5a19129a3fc827372fa1f4146
Pratham Patel <prathampatel@thefossguy.com>
2025-10-06 18:00:51 +0530

    format

--- nixos-configuration/modules/services/system/custom-nixos-upgrade.nix
+++ nixos-configuration/modules/services/system/custom-nixos-upgrade.nix
@@ -38,38 +38,44 @@ in
         Type = "oneshot";
       };
 
-      script = let
+      script =
+        let
 
-        isBuilder = if config.customOptions.localCaching.buildsNixDerivations
-          # `builtins.toString true` results in `"1"` but
-          # `builtins.toString false` results in `""`
-          # so, assign values manually
-        then "true" else "false";
-      in
-      ''
-        set -xeuf -o pipefail
+          isBuilder =
+            if
+              config.customOptions.localCaching.buildsNixDerivations
+            # `builtins.toString true` results in `"1"` but
+            # `builtins.toString false` results in `""`
+            # so, assign values manually
+            then
+              "true"
+            else
+              "false";
+        in
+        ''
+          set -xeuf -o pipefail
 
-        nixosLatestGenOutPath="$(nix eval --raw /etc/nixos#nixosConfigurations.${config.networking.hostName}.config.system.build.toplevel 2>/dev/null || echo 'eval-failed')"
-        if [[ "''${nixosLatestGenOutPath}" == 'eval-failed' ]]; then
-            echo 'Could not determine the outPath for this NixOS generation'
-            exit 1
-        fi
+          nixosLatestGenOutPath="$(nix eval --raw /etc/nixos#nixosConfigurations.${config.networking.hostName}.config.system.build.toplevel 2>/dev/null || echo 'eval-failed')"
+          if [[ "''${nixosLatestGenOutPath}" == 'eval-failed' ]]; then
+              echo 'Could not determine the outPath for this NixOS generation'
+              exit 1
+          fi
 
-        if [[ -d "''${nixosLatestGenOutPath}" ]]; then
-            echo 'This NixOS generation is already built, exiting early'
-            exit 0
-        fi
+          if [[ -d "''${nixosLatestGenOutPath}" ]]; then
+              echo 'This NixOS generation is already built, exiting early'
+              exit 0
+          fi
 
-        if [[ ${isBuilder} == 'false' ]]; then
-            nixosToplevelIsCached="$(nix path-info --store https://nix-cache.thefossguy.com "''${nixosLatestGenOutPath}" 2>/dev/null || echo 'not-cached')"
-            if [[ "''${nixosToplevelIsCached}" == 'not-cached' ]]; then
-                echo 'This NixOS generation is not cached, yet'
-                exit 1
-            fi
-        fi
+          if [[ ${isBuilder} == 'false' ]]; then
+              nixosToplevelIsCached="$(nix path-info --store https://nix-cache.thefossguy.com "''${nixosLatestGenOutPath}" 2>/dev/null || echo 'not-cached')"
+              if [[ "''${nixosToplevelIsCached}" == 'not-cached' ]]; then
+                  echo 'This NixOS generation is not cached, yet'
+                  exit 1
+              fi
+          fi
 
-        nixos-rebuild boot --show-trace --print-build-logs --flake /etc/nixos#${config.networking.hostName}
-      '';
+          nixos-rebuild boot --show-trace --print-build-logs --flake /etc/nixos#${config.networking.hostName}
+        '';
     };
   };
 }
