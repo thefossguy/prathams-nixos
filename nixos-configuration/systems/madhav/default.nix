6a7c862a70e707569e26f917f20034dc5607b199
Pratham Patel <prathampatel@thefossguy.com>
2025-10-16 00:05:37 +0530

    systems/madhav: share zpool over samba

--- nixos-configuration/systems/madhav/default.nix
+++ nixos-configuration/systems/madhav/default.nix
@@ -10,5 +10,22 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      "share" = {
+        "path" = "/heathen_disk";
+        "browseable" = "yes";
+        "read only" = "yes";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = nixosSystemConfig.coreConfig.systemUser.username;
+        "force group" = nixosSystemConfig.coreConfig.systemUser.username;
+      };
+    };
+  };
+
   customOptions.x86CpuVendor = "amd";
 }
