bb467a3309fb59a748de0a6fe3157d5ea09364b3
Pratham Patel <prathampatel@thefossguy.com>
2025-11-10 09:24:31 +0530

    home-manager: disable GPU driver integration

--- nixos-configuration/modules/home-manager/linux-home.nix
+++ nixos-configuration/modules/home-manager/linux-home.nix
@@ -10,4 +10,9 @@
 lib.mkIf pkgs.stdenv.isLinux {
   home.homeDirectory = "/home/${nixosSystemConfig.coreConfig.systemUser.username}";
   targets.genericLinux.enable = true;
+
+  # home-manager is used as an environment manager, not another nixos
+  # for the user, so disable this nonsense which also causes evaluation
+  # errors on aarch64-linux
+  targets.genericLinux.gpu = lib.mkForce false;
 }
