b51d87e93b0f5dd206c2525f5a01cc7fc103169e
Pratham Patel <prathampatel@thefossguy.com>
2025-12-22 18:21:17 +0530

    systems/hans: enable the git mirroring service

--- nixos-configuration/systems/hans/default.nix
+++ nixos-configuration/systems/hans/default.nix
@@ -11,12 +11,32 @@
   imports = [
     ../../modules/host-modules/firewall-rules.nix
     ../../modules/qemu/qemu-guest.nix
+    ../../modules/services/user/git-mirroring.nix
     ./hardware-configuration.nix
   ];
 
   boot.kernelParams = [ "console=tty" ];
   services.nix-serve.secretKeyFile = lib.mkForce null;
   zramSwap.enable = lib.mkForce false;
+
+  # setup secondary user for SSH pushes
+  users = {
+    groups."git" = {
+      name = "git";
+      gid = 9000;
+    };
+    users."git" = {
+      createHome = true;
+      group = "git";
+      hashedPassword = "!"; # disable password loggin in
+      home = "/home/git";
+      isNormalUser = true;
+      isSystemUser = false;
+      linger = true;
+      uid = 9000;
+      useDefaultShell = true;
+    };
+  };
 
   customOptions = {
     dhcpConfig = "ipv6";
