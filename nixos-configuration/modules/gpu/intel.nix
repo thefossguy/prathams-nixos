41f161a43fd49ffe87fb1f53069e11e45b0c1b28
Pratham Patel <prathampatel@thefossguy.com>
2026-02-19 12:45:25 +0530

    modules/gpu: intel: fix eval+build error and add improvements
    Switch `services.xserver.videoDrivers` from `xe` to `modesetting`
    per the NixOS Wiki about Intel Graphics[0]. Also declare
    `LIBVA_DRIVER_NAME`.
    
    [0]: <https://wiki.nixos.org/w/index.php?title=Intel_Graphics&oldid=27753>

--- nixos-configuration/modules/gpu/intel.nix
+++ nixos-configuration/modules/gpu/intel.nix
@@ -8,13 +8,23 @@
 }:
 
 lib.mkIf (builtins.elem "intel" config.customOptions.gpuSupport) {
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
   services.xserver.videoDrivers =
-    if ((builtins.elemAt config.customOptions.gpuSupport 0) == "intel") then (lib.mkBefore [ "xe" ]) else [ "xe" ];
-  hardware.intel-gpu-tools.enable = true;
-  hardware.graphics.extraPackages = with pkgs; [
-    #intel-compute-runtime # not really necessary unless OpenCL is in use
-    intel-media-driver
-    intel-vaapi-driver
-    vpl-gpu-rt
-  ];
+    if ((builtins.elemAt config.customOptions.gpuSupport 0) == "intel") then
+      (lib.mkBefore [ "modesetting" ])
+    else
+      [ "modesetting" ];
+
+  hardware = {
+    enableRedistributableFirmware = lib.mkForce true;
+    intel-gpu-tools.enable = true;
+    graphics = {
+      extraPackages = with pkgs; [
+        #intel-compute-runtime # not really necessary unless OpenCL is in use
+        intel-media-driver
+        intel-vaapi-driver
+        vpl-gpu-rt
+      ];
+    };
+  };
 }
