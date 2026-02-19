cc34c79382790d0a1de0c4ef0d8f9c925f94cd1e
Pratham Patel <prathampatel@thefossguy.com>
2026-02-19 14:43:32 +0530

    switch away from systemd-resolved to unbound

--- nixos-configuration/modules/base-config/network-configuration.nix
+++ nixos-configuration/modules/base-config/network-configuration.nix
@@ -13,26 +13,46 @@
     wait-online.enable = lib.mkForce false; # handled by my custom service
   };
 
-  environment.etc."resolv.conf".mode = "direct-symlink";
-  services.resolved = {
-    enable = true;
-    settings.Resolve = {
-      DNSSEC = "true";
-      DNSOverTLS = "true";
-      FallbackDNS = [
-        "8.8.8.8"
-        "8.8.4.4"
-      ];
+  services.resolved.enable = lib.mkForce false;
+  services.unbound = {
+    enable = lib.mkForce true;
+    settings = {
+      forward-zone = lib.mkForce [ ]; # Enforce "full recursive mode" by assigning an empty list here
+      server = {
+        interface = [ "127.0.0.1" ];
+        access-control = [ "127.0.0.1 allow" ];
+        # Some options were taken from <https://docs.pi-hole.net/guides/dns/unbound/#configure-unbound>
+        harden-glue = true;
+        harden-dnssec-stripped = true;
+        prefetch = true;
+        edns-buffer-size = 1232;
+        use-caps-for-id = false;
+        hide-identity = true;
+        hide-version = true;
+
+        # A `dig +dnssec DS fastly.net @1.1.1.1` shows that `NSEC3` is
+        # being returned. Which is proof of non-existence [of DNSSEC].
+        # And yes, this isn't handled by the `harden-dnssec-stripped`
+        # option.
+        domain-insecure = "fastly.net";
+      };
     };
   };
+  assertions = [
+    {
+      assertion = !(config.services.resolved.enable);
+      message = "Please toggle off `services.resolved.enable`.";
+    }
+    {
+      assertion = config.services.unbound.enable;
+      message = "Please toggle on `services.unbound.enable`.";
+    }
+  ];
 
   networking = {
     dhcpcd.persistent = true;
     enableIPv6 = false;
-    nameservers = [
-      "1.1.1.1"
-      "1.0.0.1"
-    ];
+    nameservers = lib.mkForce [ "127.0.0.1" ];
     tcpcrypt.enable = lib.mkForce false;
     useDHCP = lib.mkDefault true;
     wireless.enable = lib.mkForce false; # This enables `wpa_supplicant`, use `networkmanager` instead.
@@ -44,7 +64,7 @@
 
     networkmanager = {
       enable = true;
-      dns = "systemd-resolved";
+      dns = "none";
       wifi.backend = "iwd";
       connectionConfig = {
         "ipv4.ignore-auto-dns" = true;
