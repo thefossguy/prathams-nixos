856d70ffecb5b63929fd41809fb839404149ff20
Pratham Patel <prathampatel@thefossguy.com>
2026-03-18 19:59:16 +0530

    packages/navya-ci: 0.2.1 -> 0.2.2

--- nixos-configuration/packages/out-of-tree-derivations/navya-ci.nix
+++ nixos-configuration/packages/out-of-tree-derivations/navya-ci.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "navya-ci";
-  version = "0.2.1";
+  version = "0.2.2";
 
   src = fetchFromGitHub {
     owner = "thefossguy";
     repo = "navya-ci";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-4S8Irtu+JcZsSxCBHiPh3r/e5gfUKUwGVsImRBsofU8=";
+    hash = "sha256-6E11J+Io723LnKb8rXgxuDPsWazOie9poUCv3YSuUi0=";
   };
 
-  cargoHash = "sha256-yYZSpdG0iBkLBCsdXGjKD4Sgbnaiy57G0XeVFkj5Qho=";
+  cargoHash = "sha256-yu2BwaGRhCisxFbXU2y1/9YuYCiFql3YAb+orNcYnl0=";
 
   meta = {
     homepage = "https://codeberg.org/thefossguy/navya-ci";
