1737685fa62d52f2581c3773c8ab2d12f19eb57a
Pratham Patel <prathampatel@thefossguy.com>
2026-04-04 11:01:14 +0530

    services/verify-nix-store-paths: switch from `nix store` to `nix-store`

--- nixos-configuration/modules/services/system/verify-nix-store-paths.nix
+++ nixos-configuration/modules/services/system/verify-nix-store-paths.nix
@@ -24,30 +24,11 @@ lib.mkIf (!config.customOptions.localCaching.servesNixDerivations) {
       before = serviceConfig.beforeUnits;
       requiredBy = serviceConfig.requiredByUnits;
 
-      path = [ pkgs.nix ];
-
       serviceConfig = {
         User = "root";
         Type = "oneshot";
+        ExecStart = "${pkgs.nix}/bin/nix-store --verify --check-contents --repair";
       };
-
-      script = ''
-        set -x
-        set +e
-
-        nix store verify --all --recursive >/dev/null 2>&1
-        returnCode=$?
-        if [[ "''${returnCode}" -ne 0 ]] || [[ "''${returnCode}" -ne 2 ]] || [[ "''${returnCode}" -ne 4 ]]; then
-            # 0 is successful
-            # 2 is untrusted (no signatures, probably built locally)
-            # 4 encountered I/O error
-            exit 0
-        else
-            echo 'Some nix store paths are corrupted, attempting to verify'
-            set -e
-            nix store repair --all --recursive
-        fi
-      '';
     };
   };
 }
