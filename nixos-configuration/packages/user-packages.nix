22ff84b8da86ad5f7f5eff381c48e839499e878d
Pratham Patel <prathampatel@thefossguy.com>
2026-04-21 01:36:32 +0530

    pkgs: disable yt-dlp because deno has flaky builds

--- nixos-configuration/packages/user-packages.nix
+++ nixos-configuration/packages/user-packages.nix
@@ -246,7 +246,7 @@ in
     tealdeer.enable = !useMinimalConfig;
     # I randomly select a server to "download" a big [set] of video file(s)
     # so enabling `yt-dlp` is necessary.
-    yt-dlp.enable = true;
+    #yt-dlp.enable = true;
     zoxide.enable = true;
 
     direnv = lib.attrsets.optionalAttrs (!useMinimalConfig) {
