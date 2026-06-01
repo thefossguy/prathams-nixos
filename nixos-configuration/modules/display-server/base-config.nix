466797fc972f06dd222079bfd66b1322fd2052a9
Pratham Patel <prathampatel@thefossguy.com>
2026-06-01 23:39:31 +0530

    firefox: remove bitwarden extension

--- nixos-configuration/modules/display-server/base-config.nix
+++ nixos-configuration/modules/display-server/base-config.nix
@@ -186,12 +186,6 @@ lib.mkIf (config.customOptions.displayServer.guiSession != "unset") {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
             installation_mode = "force_installed";
           };
-
-          # Bitwarden
-          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-            installation_mode = "force_installed";
-          };
         };
       };
     };
