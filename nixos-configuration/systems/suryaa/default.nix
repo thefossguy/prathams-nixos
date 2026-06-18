3e9ad37e647b74b8470a97f555733844e89177b8
Pratham Patel <prathampatel@thefossguy.com>
2026-06-18 16:39:38 +0530

    systems/suryaa: add software configuration for ConnectX-7 ports

--- nixos-configuration/systems/suryaa/default.nix
+++ nixos-configuration/systems/suryaa/default.nix
@@ -10,5 +10,42 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
+  systemd.network.networks = {
+    "11-enx30c599b9ea5c" = {
+      matchConfig.Name = "enx30c599b9ea5c";
+      matchConfig.MACAddress = "30:c5:99:b9:ea:5c";
+      networkConfig.Addresses = [ "192.168.2.2/24" ];
+    };
+    "11-enx30c599b9ea60" = {
+      matchConfig.Name = "enx30c599b9ea60";
+      matchConfig.MACAddress = "30:c5:99:b9:ea:60";
+      networkConfig.Addresses = [ "192.168.3.2/24" ];
+    };
+    "11-enx30c599b9ea5d" = {
+      matchConfig.Name = "enx30c599b9ea5d";
+      matchConfig.MACAddress = "30:c5:99:b9:ea:5d";
+      networkConfig.Addresses = [ "192.168.4.1/24" ];
+    };
+    "11-enx30c599b9ea61" = {
+      matchConfig.Name = "enx30c599b9ea61";
+      matchConfig.MACAddress = "30:c5:99:b9:ea:61";
+      networkConfig.Addresses = [ "192.168.5.1/24" ];
+    };
+  };
+
+  networking.networkmanager.unmanaged = [
+    "interface-name:enx30c599b9ea5c"
+    "mac:30:c5:99:b9:ea:5c"
+
+    "interface-name:enx30c599b9ea60"
+    "mac:30:c5:99:b9:ea:60"
+
+    "interface-name:enx30c599b9ea5d"
+    "mac:30:c5:99:b9:ea:5d"
+
+    "interface-name:enx30c599b9ea61"
+    "mac:30:c5:99:b9:ea:61"
+  ];
+
   customOptions.socSupport.armSoc = "gb10";
 }
