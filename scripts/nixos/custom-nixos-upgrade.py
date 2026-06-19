399c31d377b5dfde3fdfcfb994735a513537645f
Pratham Patel <prathampatel@thefossguy.com>
2026-06-19 21:11:17 +0530

    scripts/custom-nixos-upgrade.py: init

---
+++ scripts/nixos/custom-nixos-upgrade.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+import os
+import shutil
+import subprocess
+import sys
+import time
+
+
+nixos_machine_hostname = os.environ.get("NIXOS_MACHINE_HOSTNAME", None)
+nixos_config_repo_path = "/etc/nixos"
+nixos_config_repo_url = "https://gitlab.com/thefossguy/prathams-nixos.git"
+nix_cache = "https://nix-cache.thefossguy.com"
+
+
+def re_clone_nixos_config_repo() -> None:
+    git_clone_process = subprocess.run(
+        [
+            "git",
+            "clone",
+            nixos_config_repo_path,
+            nixos_config_repo_url,
+        ],
+        check=False,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    if git_clone_process.returncode != 0:
+        logging.error(
+            f"Could not clone `{nixos_config_repo_url}` at `{nixos_config_repo_path}`",
+        )
+        sys.exit(1)
+
+
+def ensure_nixos_config_repo_integrity() -> None:
+    git_status_process = subprocess.run(
+        [
+            "git",
+            "-C",
+            nixos_config_repo_path,
+            "status",
+        ],
+        check=False,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    if git_status_process.returncode != 0:
+        logging.warning(
+            f"The NixOS configuration repository's (`{nixos_config_repo_path}`) status was not okay",
+        )
+        try:
+            shutil.rmtree(nixos_config_repo_path)
+        except (FileNotFoundError, NotADirectoryError):
+            raise
+        except Exception:
+            pass
+        re_clone_nixos_config_repo()
+
+
+def pull_nixos_config_changes() -> None:
+    git_pull_process = subprocess.run(
+        [
+            "git",
+            "-C",
+            nixos_config_repo_path,
+            "pull",
+            "--no-rebase",
+        ],
+        check=False,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    if git_pull_process.returncode != 0:
+        logging.warning(
+            f"Could not pull changes for NixOS configuration repository (`{nixos_config_repo_path}`)",
+        )
+        try:
+            shutil.rmtree(nixos_config_repo_path)
+        except (FileNotFoundError, NotADirectoryError):
+            raise
+        except Exception:
+            pass
+        re_clone_nixos_config_repo()
+
+
+def pre_start_checks() -> None:
+    error_out = False
+
+    if os.getuid() != 0:
+        logging.error("The script must run as `root`")
+        error_out = True
+
+    if not nixos_machine_hostname:
+        logging.error("$NIXOS_MACHINE_HOSTNAME is empty")
+        error_out = True
+
+    if error_out:
+        sys.exit(1)
+
+    ensure_nixos_config_repo_integrity()
+    pull_nixos_config_changes()
+
+
+def update_lockfile() -> None:
+    flake_lockfile_delta = time.time() - os.path.getmtime(
+        f"{nixos_config_repo_path}/flake.lock",
+    )
+    if flake_lockfile_delta > 55:
+        nix_flake_update_process = subprocess.run(
+            [
+                "nix",
+                "flake",
+                "update",
+                "--flake",
+                nixos_config_repo_path,
+            ],
+            check=False,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        if nix_flake_update_process.returncode != 0:
+            logging.warning("Could not update the lockfile")
+    else:
+        logging.info("Not updating flake.lock to be under GitHub's free rate limit.")
+
+
+def get_flake_store_path() -> str:
+    update_lockfile()
+    nix_flake_archive_process = subprocess.run(
+        ["nix", "flake", "archive", "--no-pretty", "--json", nixos_config_repo_path],
+        check=False,
+        text=True,
+        capture_output=True,
+    )
+    if nix_flake_archive_process.stdout:
+        flake_store_path = json.loads(nix_flake_archive_process.stdout).get("path")
+    if not flake_store_path:
+        logging.error(
+            f"Could not determine the flake store path of `{nixos_config_repo_path}`",
+        )
+        sys.exit(1)
+    else:
+        return flake_store_path
+
+
+def evaluate_latest_nixos_generation_outpath(
+    flake_store_path: str,
+) -> str:
+    nix_eval_process = subprocess.run(
+        [
+            "nix-instantiate",
+            "--eval",
+            "--raw",
+            "--expr",
+            f'(builtins.getFlake "{flake_store_path}").outputs.nixosConfigurations.{nixos_machine_hostname}.config.system.build.toplevel.outPath',
+        ],
+        check=False,
+        text=True,
+        capture_output=True,
+    )
+    if nix_eval_process.returncode != 0:
+        logging.error(
+            f"Could not determine the derivation path for `{nixos_machine_hostname}`'s NixOS configuration",
+        )
+        sys.exit(1)
+    else:
+        return nix_eval_process.stdout.strip()
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    pre_start_checks()
+
+    flake_store_path = get_flake_store_path()
+    logging.info(f"Flake store path: '{flake_store_path}'")
+
+    latest_nixos_generation_outpath = evaluate_latest_nixos_generation_outpath(
+        flake_store_path,
+    )
+    logging.info(f"Latest NixOS generation path: '{latest_nixos_generation_outpath}'")
+
+    if os.path.realpath("/nix/var/nix/profiles/system") == latest_nixos_generation_outpath:
+        logging.info(
+            f"`{latest_nixos_generation_outpath}` is already the latest NixOS generation, nothing to do",
+        )
+    else:
+        nix_build_process = subprocess.run(
+            [
+                "nix",
+                "build",
+                "--no-link",
+                "--keep-going",
+                "--max-jobs",
+                "0",
+                latest_nixos_generation_outpath,
+            ],
+            check=False,
+            text=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        if nix_build_process.returncode != 0:
+            logging.warning(
+                f"`{latest_nixos_generation_outpath}` is not fully cached yet",
+            )
+            sys.exit(0)
+        else:
+            nixos_rebuild_process = subprocess.run(
+                [
+                    "nixos-rebuild",
+                    "boot",
+                    "--show-trace",
+                    "--print-build-logs",
+                    "--flake",
+                    f"{flake_store_path}#{nixos_machine_hostname}",
+                ],
+                check=False,
+            )
+            sys.exit(nixos_rebuild_process.returncode)
+
+
+if __name__ == "__main__":
+    main()
