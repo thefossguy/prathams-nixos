448a08ee620ced2c985497361ada88650cfc440b
Pratham Patel <prathampatel@thefossguy.com>
2026-07-26 16:22:39 +0530

    systems/indra: add luks configuration

--- nixos-configuration/systems/indra/hardware-configuration.nix
+++ nixos-configuration/systems/indra/hardware-configuration.nix
@@ -8,19 +8,58 @@
 }:
 
 {
+  customOptions = {
+    fileSystems = {
+      UUIDs = {
+        boot = "3F17-D0CC";
+        root = "ec897753-a6cf-464d-ac29-070085443395";
+      };
+    };
+
+    luksDevice = {
+      UUID = "d49ebdeb-6bbc-4175-827a-5f44859942c3";
+      bypassWorkqueues = true;
+      challengeStringHex = "401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429080fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1";
+    };
+  };
+
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/2C9D-5832";
+    device = config.customOptions.fileSystems.devices.boot;
   };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/ee12bd22-12e0-4840-8f16-46a9540bfbac";
+    device = config.customOptions.fileSystems.devices.root;
+    fsType = lib.mkForce "btrfs";
+    options = [
+      "subvol=@"
+      "compress=zstd:15"
+    ];
+  };
+
+  fileSystems."/nix" = {
+    device = config.customOptions.fileSystems.devices.root;
+    fsType = lib.mkForce "btrfs";
+    options = [
+      "subvol=@nix"
+      "compress=zstd:15"
+    ];
   };
 
   fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/cffc2a40-66a3-4ae2-96f8-f6b9a55c12c7";
+    device = config.customOptions.fileSystems.devices.root;
+    fsType = lib.mkForce "btrfs";
+    options = [
+      "subvol=@home"
+      "compress=zstd:15"
+    ];
   };
 
   fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/e19a752e-92e8-476a-b5e7-a9c16373fde5";
+    device = config.customOptions.fileSystems.devices.root;
+    fsType = lib.mkForce "btrfs";
+    options = [
+      "subvol=@var"
+      "compress=zstd:15"
+    ];
   };
 }
