30de45c04aff7168c5f435906065abe35d57e315
Pratham Patel <prathampatel@thefossguy.com>
2026-08-02 21:13:32 +0530

    systems/mahadev: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/mahadev/hardware-configuration.nix
+++ nixos-configuration/systems/mahadev/hardware-configuration.nix
@@ -8,19 +8,20 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/B911-786F";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/83cf9d36-a97a-4336-ab3c-e818cc4edc30";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/b8a89e3a-f6d9-456c-a554-6d6fd498ce46";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/7cb3c69f-2c62-4498-86e5-f168b3f2daa4";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    fileSystemsOnRootfsDevice = [
+      "/"
+      "/boot"
+      "/home"
+      "/nix"
+      "/persistent/data"
+      "/persistent/state"
+      "/var"
+    ];
+    UUIDs = {
+      boot = "1BC0-F0DA";
+      root = "96a4d247-e5fe-44ad-9e44-66b24037beb8";
+    };
   };
 }
