2aa5ed3d18cf4dbe9ca0769b7ff56ad98ec74dc4
Pratham Patel <prathampatel@thefossguy.com>
2026-08-02 14:40:48 +0530

    nix-serve/harmonia: move config out of the binary-cache-server config into a dedicated file

---
+++ nixos-configuration/modules/local-nix-cache/binary-cache.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  pkgs,
+  stablePkgs,
+  nixosSystemConfig,
+  ...
+}:
+
+let
+  binaryCacheIface = "127.0.0.1";
+  binaryCachePort = "5000";
+in
+
+{
+  services.harmonia = {
+    cache = {
+      enable = true;
+      settings = {
+        priority = 10;
+        enable_compression = true;
+        bind = "${binaryCacheIface}:${binaryCachePort}";
+        workers = 4; # all my machines have at least 4 cores.
+        max_connection_rate = 256;
+      };
+    };
+    daemon = {
+      enable = true;
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 ];
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts = {
+      "nixcache.${config.networking.hostName}.localhost" = {
+        locations."/".proxyPass = "http://${binaryCacheIface}:${binaryCachePort}";
+      };
+    };
+  };
+}
--- nixos-configuration/modules/local-nix-cache/server.nix
+++ nixos-configuration/modules/local-nix-cache/server.nix
@@ -7,37 +7,6 @@
   ...
 }:
 
-let
-  binaryCacheIface = "127.0.0.1";
-  binaryCachePort = "5000";
-in
-
 lib.mkIf config.customOptions.localCaching.servesNixDerivations {
-  services.harmonia = {
-    cache = {
-      enable = true;
-      settings = {
-        priority = 10;
-        enable_compression = true;
-        bind = "${binaryCacheIface}:${binaryCachePort}";
-        workers = 4; # all my machines have at least 4 cores.
-        max_connection_rate = 256;
-      };
-    };
-    daemon = {
-      enable = true;
-    };
-  };
-
-  networking.firewall.allowedTCPPorts = [ 80 ];
-
-  services.nginx = {
-    enable = true;
-    recommendedProxySettings = true;
-    virtualHosts = {
-      "nixcache.${config.networking.hostName}.localhost" = {
-        locations."/".proxyPass = "http://${binaryCacheIface}:${binaryCachePort}";
-      };
-    };
-  };
+  imports = [ ./binary-cache.nix ];
 }
