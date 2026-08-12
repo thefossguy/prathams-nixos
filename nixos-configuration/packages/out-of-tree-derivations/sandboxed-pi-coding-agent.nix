6bd0f1072bfb55959f4ca8f39b0432a7d5b5f1db
Pratham Patel <prathampatel@thefossguy.com>
2026-08-13 01:40:00 +0530

    overlays/sandboxed-pi-coding-agent: set PI_CODING_AGENT_DIR

--- nixos-configuration/packages/out-of-tree-derivations/sandboxed-pi-coding-agent.nix
+++ nixos-configuration/packages/out-of-tree-derivations/sandboxed-pi-coding-agent.nix
@@ -13,5 +13,6 @@ writeScriptBin "sandboxed-pi-coding-agent" ''
   ${lib.getExe pi-coding-agent-sandboxer} \
       --setpriv-bin ${lib.getExe' util-linux "setpriv"} \
       --pi-bin ${lib.getExe pi-coding-agent} \
+      --pi-env PI_CODING_AGENT_DIR='~/.config/pi/agent' \
       "$@"
 ''
