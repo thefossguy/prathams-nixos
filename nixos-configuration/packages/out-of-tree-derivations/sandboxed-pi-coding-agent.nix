22e8f94b726e476fbb0f5e976419783af2dfbd34
Pratham Patel <prathampatel@thefossguy.com>
2026-08-21 13:55:15 +0530

    overlays/sandboxed-pi-coding-agent: switch from `pi-coding-agent-sandboxer` to `clanker-jail`

--- nixos-configuration/packages/out-of-tree-derivations/sandboxed-pi-coding-agent.nix
+++ nixos-configuration/packages/out-of-tree-derivations/sandboxed-pi-coding-agent.nix
@@ -2,17 +2,12 @@
   writeScriptBin,
   lib,
   bash,
-  pi-coding-agent-sandboxer,
-  util-linux,
+  clanker-jail,
   pi-coding-agent,
 }:
 
 writeScriptBin "sandboxed-pi-coding-agent" ''
   #!${lib.getExe bash}
 
-  ${lib.getExe pi-coding-agent-sandboxer} \
-      --setpriv-bin ${lib.getExe' util-linux "setpriv"} \
-      --pi-bin ${lib.getExe pi-coding-agent} \
-      --pi-env PI_CODING_AGENT_DIR='~/.config/pi/agent' \
-      "$@"
+  exec ${lib.getExe clanker-jail} --clanker ${lib.getExe pi-coding-agent} "$@"
 ''
