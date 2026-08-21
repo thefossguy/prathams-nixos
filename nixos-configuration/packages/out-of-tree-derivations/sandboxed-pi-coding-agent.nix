510dcdf9587aca5abdddbea4a51dbaf4f49af834
Pratham Patel <prathampatel@thefossguy.com>
2026-08-21 14:28:18 +0530

    overlays/sandboxed-pi-coding-agent: use $HOME/.tmp/clanker-tmpdir as $TMPDIR

--- nixos-configuration/packages/out-of-tree-derivations/sandboxed-pi-coding-agent.nix
+++ nixos-configuration/packages/out-of-tree-derivations/sandboxed-pi-coding-agent.nix
@@ -9,5 +9,6 @@
 writeScriptBin "sandboxed-pi-coding-agent" ''
   #!${lib.getExe bash}
 
+  export TMPDIR=$HOME/.tmp/clanker-tmpdir
   exec ${lib.getExe clanker-jail} --clanker ${lib.getExe pi-coding-agent} "$@"
 ''
