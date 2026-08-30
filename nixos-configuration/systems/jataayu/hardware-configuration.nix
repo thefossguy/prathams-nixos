0f3e9246bfe92566c3c06bc98daf317d084f629b
Pratham Patel <prathampatel@thefossguy.com>
2026-08-30 18:41:40 +0530

    systems/jataayu: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/jataayu/hardware-configuration.nix
+++ nixos-configuration/systems/jataayu/hardware-configuration.nix
@@ -8,19 +8,20 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/EE6F-50F8";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/c9324356-31e5-4d18-80a4-2d578575c892";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/e990d2b9-7d1c-4d6b-923a-d8e538fb184e";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/a3433bc6-0ac7-4233-832d-fbce7f184b0d";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    fileSystemsOnRootfsDevice = [
+      "/"
+      "/boot"
+      "/home"
+      "/nix"
+      "/persistent/data"
+      "/persistent/state"
+      "/var"
+    ];
+    UUIDs = {
+      boot = "02D2-405F";
+      root = "84639c9b-8ac4-49f2-aac0-136ae1ca5cf2";
+    };
   };
 }
