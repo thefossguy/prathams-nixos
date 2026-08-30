b1189d3b9ff50dc2b7e7d47733be8aceefd2a918
Pratham Patel <prathampatel@thefossguy.com>
2026-08-30 20:11:01 +0530

    services/rollback-root: init

--- nixos-configuration/modules/host-modules/filesystem-configuration.nix
+++ nixos-configuration/modules/host-modules/filesystem-configuration.nix
@@ -56,6 +56,59 @@ let
 in
 {
 
+  boot.initrd.systemd.services.rollback-root = {
+    description = "Rollback rootfs to a blank snapshot";
+    wantedBy = [ "initrd.target" ];
+    before = [ "sysroot.mount" ];
+    after = [ "systemd-udev-settle.service" ];
+    unitConfig.defaultDependencies = false;
+    serviceConfig.type = "oneshot";
+
+    script =
+      let
+        commonPkgs = with pkgs; [
+          coreutils-full
+          util-linux
+        ];
+        rollbackScript = {
+          xfs = ''
+            # intentionally left empty; snapshotting (and therefore rollbacks) are unsupported on XFS
+          '';
+
+          zfs =
+            let
+              fsProgs = commonPkgs ++ [ config.boot.kernelPackages.${pkgs.zfs.kernelModuleAttribute}.userspaceTools ];
+            in
+            ''
+              export PATH=${lib.makeBinPath (builtins.map (pkg: pkg.out or pkg) fsProgs)}:$PATH
+            '';
+
+          btrfs =
+            let
+              fsProgs = commonPkgs ++ [ pkgs.btrfs-progs ];
+            in
+            ''
+              export PATH=${lib.makeBinPath (builtins.map (pkg: pkg.out or pkg) fsProgs)}:$PATH
+
+              mkdir --verbose --parents /btrfs/old
+              mount --options ${
+                builtins.concatStringsSep "," config.fileSystems."/".options
+              } ${config.fileSystems."/".device} /btrfs/old
+
+              if [ -d '/btrfs/old/@+fresh' ]; then
+                  btrfs subvolume delete '/btrfs/old@'
+                  btrfs subvolume snapshot '/btrfs/old@+fresh' '/btrfs/old@'
+              else
+                  echo 'rollback-root: @+fresh snapshot not found, aborting rollback' >&2
+              fi
+
+              umount --verbose --recursive /btrfs/old
+            '';
+        };
+      in
+      rollbackScript.${config.fileSystems."/".fsType} or null;
+  };
+
   fileSystems =
     if (config.customOptions.fileSystems.rootFileSystem != "xfs") then
       (builtins.foldl' (
