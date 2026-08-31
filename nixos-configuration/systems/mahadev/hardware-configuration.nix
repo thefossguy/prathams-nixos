075fd04dab753fd52a4ae9226b2b09be09314df0
Pratham Patel <prathampatel@thefossguy.com>
2026-08-31 18:14:57 +0530

    systems/mahadev: we no longer need to specify `customOptions.fileSystems.fileSystemsOnRootfsDevice`

--- nixos-configuration/systems/mahadev/hardware-configuration.nix
+++ nixos-configuration/systems/mahadev/hardware-configuration.nix
@@ -10,15 +10,6 @@
 {
   customOptions.fileSystems = {
     rootFileSystem = "btrfs";
-    fileSystemsOnRootfsDevice = [
-      "/"
-      "/boot"
-      "/home"
-      "/nix"
-      "/persistent/data"
-      "/persistent/state"
-      "/var"
-    ];
     UUIDs = {
       boot = "1BC0-F0DA";
       root = "96a4d247-e5fe-44ad-9e44-66b24037beb8";
