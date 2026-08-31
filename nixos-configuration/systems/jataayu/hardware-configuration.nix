25d646e94239dc1e5479341946f54b6ff24354a8
Pratham Patel <prathampatel@thefossguy.com>
2026-08-31 18:14:57 +0530

    systems/jataayu: we no longer need to specify `customOptions.fileSystems.fileSystemsOnRootfsDevice`

--- nixos-configuration/systems/jataayu/hardware-configuration.nix
+++ nixos-configuration/systems/jataayu/hardware-configuration.nix
@@ -10,15 +10,6 @@
 {
   customOptions.fileSystems = {
     rootFileSystem = "btrfs";
-    fileSystemsOnRootfsDevice = [
-      "/"
-      "/boot"
-      "/home"
-      "/nix"
-      "/persistent/data"
-      "/persistent/state"
-      "/var"
-    ];
     UUIDs = {
       boot = "02D2-405F";
       root = "84639c9b-8ac4-49f2-aac0-136ae1ca5cf2";
