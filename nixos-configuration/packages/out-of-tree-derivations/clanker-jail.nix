0a72eeb11fff8412ce5e5c9294d2e1ad5eb45603
Pratham Patel <prathampatel@thefossguy.com>
2026-09-04 22:34:55 +0530

    overlays/clanker-jail: 0.1.0-unstable-2026-09-04 -> 0.1.0-unstable-2026-09-04-e6fbdd

--- nixos-configuration/packages/out-of-tree-derivations/clanker-jail.nix
+++ nixos-configuration/packages/out-of-tree-derivations/clanker-jail.nix
@@ -6,13 +6,13 @@
 
 rustPlatform.buildRustPackage {
   pname = "clanker-jail";
-  version = "0.1.0-unstable-2026-09-04";
+  version = "0.1.0-unstable-2026-09-04-e6fbdd";
 
   src = fetchFromCodeberg {
     owner = "thefossguy";
     repo = "clanker-jail";
-    rev = "a9f9c50dd37bb1d4bee09c68256f9440ebe03122";
-    hash = "sha256-ZHwqSiPyrojNxvJKqlUncOoSgHj8wEgLt0hCCDnRViE=";
+    rev = "e6fbdde65873ebc7e4e9dadc8ca376b4431be781";
+    hash = "sha256-tnQeG2Xc4+F+J/jxwThDODI86r6F6GEw7tJOaX/fXv4=";
   };
 
   cargoHash = "sha256-+7lbQHuKWjRLtbsDKA0+m598XnIodOlwRgYXkT3cGdM=";
