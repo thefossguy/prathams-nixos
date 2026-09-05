02861b7b49b98c8696b85eb0e5f5cfa20c9c3b94
Pratham Patel <prathampatel@thefossguy.com>
2026-09-05 19:38:36 +0530

    nixos-host-vm-tests: disable bootloader

--- nixos-configuration/modules/nixos-host-vm-tests/default.nix
+++ nixos-configuration/modules/nixos-host-vm-tests/default.nix
@@ -21,6 +21,7 @@ let
 
             virtualisation.vmVariant.virtualisation = {
               diskImage = lib.mkForce null;
+              useBootLoader = lib.mkForce false;
               graphics = lib.mkForce false;
               memorySize = lib.mkForce 2048;
               cores = lib.mkForce 1;
