7d63e52a89789883478affedf8bacec7b39d1038
Pratham Patel <prathampatel@thefossguy.com>
2026-09-06 18:30:58 +0530

    Revert "nixos-host-vm-tests/systemd-tmpfiles-state-verifier: add timeout for the VM execution"
    This reverts commit b9f97f8f0570199495926f7213023d445329c447.

--- nixos-configuration/modules/nixos-host-vm-tests/systemd-tmpfiles-state-verifier.nix
+++ nixos-configuration/modules/nixos-host-vm-tests/systemd-tmpfiles-state-verifier.nix
@@ -55,9 +55,7 @@ builtins.mapAttrs (
       # and gets mounted in the VM at `$TMPDIR/shared`.
       export SHARED_DIR=$(mktemp -d)
 
-      # The test finishes in under ~45 seconds but lets double it + add buffer.
-      ${nixosTestVM.lib.getExe' nixosTestVM.pkgs.coreutils-full "timeout"} 120s \
-          ${nixosTestVM.lib.getExe nixosTestVM.config.system.build.vm} 2>&1 | \
+      ${nixosTestVM.lib.getExe nixosTestVM.config.system.build.vm} 2>&1 | \
           ${nixosTestVM.lib.getExe nixosTestVM.pkgs.ansifilter}
     '';
 
