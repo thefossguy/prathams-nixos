feb8bd8f481823cfc7a502238b6d99129e9d67d6
Pratham Patel <prathampatel@thefossguy.com>
2026-09-10 02:37:12 +0530

    overlays/rollbacker: 0.1.0-unstable-2026-09-09 -> 0.1.0-unstable-2026-09-10

--- nixos-configuration/packages/out-of-tree-derivations/rollbacker.nix
+++ nixos-configuration/packages/out-of-tree-derivations/rollbacker.nix
@@ -6,13 +6,13 @@
 
 rustPlatform.buildRustPackage {
   pname = "rollbacker";
-  version = "0.1.0-unstable-2026-09-09";
+  version = "0.1.0-unstable-2026-09-10";
 
   src = fetchFromCodeberg {
     owner = "thefossguy";
     repo = "rollbacker";
-    rev = "c2523337302d7598d332aa41dbf3fe3d3fe1ab14";
-    hash = "sha256-m52ANe3bj16SPogH7i4JsTHsOqH3tdWeq4zI8dvhnI4=";
+    rev = "bb13b3a124e19ee40bacebcbfa5cc3df5d41cbef";
+    hash = "sha256-gzW1Kfm0KOm7S7i8epMCTyAirADC2e2LD2V2IHwiCQE=";
   };
 
   cargoHash = "sha256-p7x39XJQVrWPCnj8uX29sBiEsXsmAYtpxvYwluhrXKI=";
