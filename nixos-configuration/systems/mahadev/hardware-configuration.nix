642ec3e0b2f78b6344b01709957de98e33cfc2fe
Pratham Patel <prathampatel@thefossguy.com>
2026-09-12 14:04:41 +0530

    systems/mahadev: update UUID for boot part

--- nixos-configuration/systems/mahadev/hardware-configuration.nix
+++ nixos-configuration/systems/mahadev/hardware-configuration.nix
@@ -11,7 +11,7 @@
   customOptions.fileSystems = {
     rootFileSystem = "btrfs";
     UUIDs = {
-      boot = "1BC0-F0DA";
+      boot = "AAFB-BF6E";
       root = "96a4d247-e5fe-44ad-9e44-66b24037beb8";
     };
   };
