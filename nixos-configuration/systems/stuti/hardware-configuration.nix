43a30112a8c87206d46bc8f46fbdf96a54525701
Pratham Patel <prathampatel@thefossguy.com>
2026-09-12 14:04:41 +0530

    systems/stuti: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/stuti/hardware-configuration.nix
+++ nixos-configuration/systems/stuti/hardware-configuration.nix
@@ -8,19 +8,11 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/B4DF-D804";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/e3c37f3e-d920-4ec3-b80a-0ce53fabe98b";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/df2f3866-9c7e-4f22-85ba-7f3ef3fe3e5c";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/8507cc8d-8f9c-4360-b617-c628cd333bde";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    UUIDs = {
+      boot = "FA10-C324";
+      root = "2c7697a0-4bcf-44f4-9c49-4b3d1c2b3496";
+    };
   };
 }
