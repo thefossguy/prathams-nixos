d5339fc3d9c01457e0df8481ab4e621345299d9a
Pratham Patel <prathampatel@thefossguy.com>
2026-09-12 14:04:41 +0530

    systems/pawandev: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/pawandev/hardware-configuration.nix
+++ nixos-configuration/systems/pawandev/hardware-configuration.nix
@@ -8,19 +8,11 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/46BD-A6DC";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/d91a6ad1-bff4-43bd-bddd-2936a4f1078b";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/a1e5cb2c-2863-4ee7-be1f-5b32f7eb9d16";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/28cc2d02-e91b-4368-81e5-d65b9d8a50f3";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    UUIDs = {
+      boot = "AF87-CC05";
+      root = "31fb239f-1410-475f-a96d-8b50db8508e4";
+    };
   };
 }
