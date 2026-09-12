4fedcec590d52f510475a8b2394e5c4929477049
Pratham Patel <prathampatel@thefossguy.com>
2026-09-12 14:04:41 +0530

    systems/bheem: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/bheem/hardware-configuration.nix
+++ nixos-configuration/systems/bheem/hardware-configuration.nix
@@ -8,19 +8,11 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/624E-EE88";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/9ce0d088-217f-431c-bf09-7f139e9bbc24";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/d1a9161e-d8c2-4bf5-981d-20efbe5804fe";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/b5818f6d-d369-4bab-82ba-402afd7a4460";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    UUIDs = {
+      boot = "F289-B3A2";
+      root = "7968695f-4449-4a8c-8797-557a9dd53244";
+    };
   };
 }
