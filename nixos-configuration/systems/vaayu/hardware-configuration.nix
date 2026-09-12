1e4275f7dd1f55f07b87e9158e25cb872b7bb078
Pratham Patel <prathampatel@thefossguy.com>
2026-09-12 14:04:41 +0530

    systems/vaayu: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/vaayu/hardware-configuration.nix
+++ nixos-configuration/systems/vaayu/hardware-configuration.nix
@@ -8,19 +8,11 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/E150-2575";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/662384e7-4d66-4f9f-bf71-f6cb2eb20ad5";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/f90fe2e2-23eb-4f96-800a-fc278fc3bc73";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/58681bd6-8b77-437e-ad4b-af9cb052cc93";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    UUIDs = {
+      boot = "AE45-F80B";
+      root = "67d7f855-29e3-4666-b9ac-cf0cf3f09cd2";
+    };
   };
 }
