77d3fea1a1124cc77eec143df5ec4ef3f8197976
Pratham Patel <prathampatel@thefossguy.com>
2026-09-12 14:04:41 +0530

    systems/suryaaa: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/suryaaa/hardware-configuration.nix
+++ nixos-configuration/systems/suryaaa/hardware-configuration.nix
@@ -8,19 +8,11 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/733C-DCA4";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/988d9091-9113-4aab-8edc-215dab26c850";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/8a8af2e4-51ed-4d35-a801-da2b8c5082ee";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/834f6c4e-a9d9-4fa3-8069-12caa6f22857";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    UUIDs = {
+      boot = "E288-BCDD";
+      root = "542e0e9f-d6c5-4ade-934f-937e3101925e";
+    };
   };
 }
