9a1fb02713e7634a84245ac2f91cd1b47335b9f1
Pratham Patel <prathampatel@thefossguy.com>
2026-09-12 14:04:41 +0530

    systems/vaaman: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/vaaman/hardware-configuration.nix
+++ nixos-configuration/systems/vaaman/hardware-configuration.nix
@@ -8,19 +8,11 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/D546-9873";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/4844363d-0dff-4dd2-88d8-704de750fe09";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/6be45b33-103b-4679-b657-a8faeec3f8f6";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/12fb6827-ac35-4400-ad9c-1763cd94e37f";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    UUIDs = {
+      boot = "B937-B0DF";
+      root = "2301186b-b974-4011-a5ee-283a1fb4120b";
+    };
   };
 }
