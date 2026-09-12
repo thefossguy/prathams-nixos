26bb0a738a1a0225c81806d5cb6f0ce6fef3a2de
Pratham Patel <prathampatel@thefossguy.com>
2026-09-12 14:04:41 +0530

    systems/reddish: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/reddish/hardware-configuration.nix
+++ nixos-configuration/systems/reddish/hardware-configuration.nix
@@ -8,19 +8,11 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/15D7-1EF4";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/c78f0691-a246-4b01-bb33-41662abcb2d6";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/9f8f26e5-5fc3-4f4a-ae45-4f24151e846e";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/cb9dcd35-891b-438f-baa1-fd3278dc3069";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    UUIDs = {
+      boot = "A359-4C61";
+      root = "970a032d-5e43-45a8-bd9d-6fb79b161662";
+    };
   };
 }
