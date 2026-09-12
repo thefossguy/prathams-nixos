20cf7913ea119d1e932053dd1d634a272bfdf082
Pratham Patel <prathampatel@thefossguy.com>
2026-09-12 14:04:41 +0530

    systems/sentinal: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/sentinel/hardware-configuration.nix
+++ nixos-configuration/systems/sentinel/hardware-configuration.nix
@@ -8,19 +8,11 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/0659-9795";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/efea71e0-6154-4fb5-b10b-f84dec50c08f";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/36ed9313-090d-4f24-995a-6c2c008e05fd";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/1a989d9a-e9d6-4762-b6d4-8c4774ec1d93";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    UUIDs = {
+      boot = "E920-66D3X";
+      root = "0e0a780c-a145-40e1-bf8c-d1d8dce72571";
+    };
   };
 }
