f564ac0ad846d75e67a8db6ca7b6be32831976e5
Pratham Patel <prathampatel@thefossguy.com>
2026-09-14 19:22:39 +0530

    systems/matsya: migrate from multi-partition xfs setup to single-partition btrfs setup

--- nixos-configuration/systems/matsya/hardware-configuration.nix
+++ nixos-configuration/systems/matsya/hardware-configuration.nix
@@ -8,19 +8,11 @@
 }:
 
 {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/2243-C1B4";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/a7b23478-37c9-4004-aac2-a7d0efde46a7";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/2ced6246-5702-4a76-a167-f90121d3e54c";
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/ccff8897-d465-44de-9f8b-1faf8dc66a63";
+  customOptions.fileSystems = {
+    rootFileSystem = "btrfs";
+    UUIDs = {
+      boot = "B633-B21E";
+      root = "456e3903-581f-46f4-b69d-62a42f785c4e";
+    };
   };
 }
