7eb23f568c3713f75a1f8b1b56b4a614175ee1c5
Pratham Patel <prathampatel@thefossguy.com>
2026-09-15 16:36:05 +0530

    systems/sentinel: set `config.customOptions.etcMachineID`

--- nixos-configuration/systems/sentinel/default.nix
+++ nixos-configuration/systems/sentinel/default.nix
@@ -11,6 +11,7 @@
   imports = [ ./hardware-configuration.nix ];
 
   customOptions = {
+    etcMachineID = "f2956c2de1d843e39fecf26613e95e4e";
     persistence.enable = true;
     socSupport.armSoc = "rpi4";
   };
