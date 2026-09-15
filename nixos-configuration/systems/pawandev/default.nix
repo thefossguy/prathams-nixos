2355848e11845d280155644bdb656e9021587029
Pratham Patel <prathampatel@thefossguy.com>
2026-09-15 16:36:05 +0530

    systems/pawandev: set `config.customOptions.etcMachineID`

--- nixos-configuration/systems/pawandev/default.nix
+++ nixos-configuration/systems/pawandev/default.nix
@@ -11,6 +11,7 @@
   imports = [ ./hardware-configuration.nix ];
 
   customOptions = {
+    etcMachineID = "f262995281654a85a8fa2fbae3b997d1";
     persistence.enable = true;
     socSupport.armSoc = "rk3588";
   };
