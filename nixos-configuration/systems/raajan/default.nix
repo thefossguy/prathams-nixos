7e6e4347b98eafaf35edbff21864ee4b3c51ce87
Pratham Patel <prathampatel@thefossguy.com>
2026-09-15 16:36:05 +0530

    systems/raajan: set `config.customOptions.etcMachineID`

--- nixos-configuration/systems/raajan/default.nix
+++ nixos-configuration/systems/raajan/default.nix
@@ -11,6 +11,7 @@
   imports = [ ./hardware-configuration.nix ];
 
   customOptions = {
+    etcMachineID = "fea08f2a25974e6c934b48f36b63d2ca";
     persistence.enable = true;
     socSupport.armSoc = "rpi5";
   };
