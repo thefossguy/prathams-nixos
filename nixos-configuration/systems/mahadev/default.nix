6cee7f0a6765b77ba6ec8ab44674eac5373d420a
Pratham Patel <prathampatel@thefossguy.com>
2026-09-15 16:36:05 +0530

    systems/mahadev: set `config.customOptions.etcMachineID`

--- nixos-configuration/systems/mahadev/default.nix
+++ nixos-configuration/systems/mahadev/default.nix
@@ -15,6 +15,7 @@
 
   customOptions = {
     displayServer.guiSession = "cosmic";
+    etcMachineID = "f6c39553a79a423e8255790a64a523a4";
     persistence.enable = true;
     socSupport.armSoc = "rk3588";
   };
