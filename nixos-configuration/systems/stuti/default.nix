f4ce422eeb6051eee4494ef965aabc189236d58f
Pratham Patel <prathampatel@thefossguy.com>
2026-09-15 16:36:05 +0530

    systems/stuti: set `config.customOptions.etcMachineID`

--- nixos-configuration/systems/stuti/default.nix
+++ nixos-configuration/systems/stuti/default.nix
@@ -11,6 +11,7 @@
   imports = [ ./hardware-configuration.nix ];
 
   customOptions = {
+    etcMachineID = "f23f95475f594760b381d2ab29395d48";
     persistence.enable = true;
     socSupport.armSoc = "rk3588";
   };
