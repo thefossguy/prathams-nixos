672fa77c8137ed34c15be60db08bb4cf8468bdf8
Pratham Patel <prathampatel@thefossguy.com>
2026-09-22 01:50:30 +0530

    soc-support/firmware-updater: start using `update-uboot-tfg`

--- nixos-configuration/modules/soc-support/z-firmware-updater.nix
+++ nixos-configuration/modules/soc-support/z-firmware-updater.nix
@@ -8,35 +8,50 @@
 }:
 
 let
-  ubootPackages = {
-    # TODO: Add support for `vaaman` and `vaayu` once nixpkgs has RISC-V support.
-    chaturvyas = pkgs.ubootCM3588NAS;
-    mahadev = pkgs.ubootRock5ModelB;
-    pawandev = pkgs.ubootOrangePi5;
-    raajan = pkgs.rpiUbootAndFirmware;
-    reddish = pkgs.rpiUbootAndFirmware;
-    sentinel = pkgs.rpiUbootAndFirmware;
-    stuti = pkgs.ubootNanoPCT6;
-  };
-  selectedUbootPackage = ubootPackages."${config.networking.hostName}" or null;
-  ddFlags = "conv=sync status=progress";
-  appendedPath = import ../../../functions/append-to-path.nix {
-    packages = with pkgs; [
-      coreutils-full
-      gnugrep
-      util-linux
-    ];
-  };
+  updateUbootTfgConfig =
+    {
+      chaturvyas = {
+        board = "cm3588";
+        ubootPath = pkgs.ubootCM3588NAS;
+      };
 
-  rpiUpdateScript =
-    lib.strings.optionalString
-      ((config.customOptions.socSupport.armSoc == "rpi4") || (config.customOptions.socSupport.armSoc == "rpi5"))
-      ''
-        ${pkgs.rsync}/bin/rsync --quiet --no-motd --checksum --recursive --progress --stats ${selectedUbootPackage.outPath}/ /boot/
-        exit 0
-      '';
+      mahadev = {
+        board = "rock-5-model-b";
+        ubootPath = pkgs.ubootRock5ModelB;
+      };
 
-  rk3588UpdateScript = lib.strings.optionalString (config.customOptions.socSupport.armSoc == "rk3588") ''
+      pawandev = {
+        board = "orange-pi-5";
+        ubootPath = pkgs.ubootOrangePi5;
+      };
+
+      raajan = {
+        board = "raspberry-pi-5-model-b";
+        ubootPath = pkgs.rpiUbootAndFirmware;
+      };
+
+      reddish = {
+        board = "raspberry-pi-4-model-b";
+        ubootPath = pkgs.rpiUbootAndFirmware;
+      };
+
+      sentinel = {
+        board = "raspberry-pi-4-model-b";
+        ubootPath = pkgs.rpiUbootAndFirmware;
+      };
+
+      stuti = {
+        board = "nanopc-t6";
+        ubootPath = pkgs.ubootNanoPCT6;
+      };
+    }
+    ."${config.networking.hostName}" or null;
+in
+
+lib.mkIf config.customOptions.socSupport.handleFirmwareUpdates {
+  boot.loader.systemd-boot.extraInstallCommands = ''
+    #!/usr/bin/env bash
+
     if [[ "$(cat /proc/sys/kernel/hostname)" != '${config.networking.hostName}' ]]; then
         echo 'Refusing to proceed further because'
         echo '1. The NixOS System is being built in a CI and updating U-Boot'
@@ -47,40 +62,6 @@ let
         exit 0
     fi
 
-    if [[ -c /dev/mtd0 ]]; then
-        dd ${ddFlags} of=/dev/mtd0 if=${selectedUbootPackage.outPath}/u-boot-rockchip-spi.bin
-        exit 0
-    fi
-
-    EMMC_DEVICES=( '/dev/mmcblk0' '/dev/mmcblk1' )
-    for MMC_DEV in "''${EMMC_DEVICES[@]}"; do
-        if [[ -b "''${MMC_DEV}" ]]; then
-            if fdisk -l "''${MMC_DEV}" | grep 'EFI System' | grep -q 131072; then
-                dd ${ddFlags} bs=512 seek=64 of="''${MMC_DEV}" if=${selectedUbootPackage.outPath}/u-boot-rockchip.bin
-                exit 0
-            fi
-        fi
-    done
-  '';
-in
-
-lib.mkIf config.customOptions.socSupport.handleFirmwareUpdates {
-  boot.loader.systemd-boot.extraInstallCommands = ''
-    # ----[ cut ]----
-    # U-Boot upgrade script starts here
-    set -xeuf -o pipefail
-
-    ${appendedPath}
-    export PATH
-
-    if grep --quiet --text '^${selectedUbootPackage.version}' '/proc/device-tree/chosen/u-boot,version'; then
-        exit 0
-    fi
-
-    ${rpiUpdateScript}
-    ${rk3588UpdateScript}
-
-    echo 'The script finished but could not flash U-Boot.'
-    exit 1
+    ${lib.getExe pkgs.update-uboot-tfg} --board ${updateUbootTfgConfig.board} --uboot-path ${updateUbootTfgConfig.ubootPath} --uboot-version ${updateUbootTfgConfig.ubootPath.version}
   '';
 }
