8d63cbbcc226c7ef5b2854816825a3673d878d8d
Pratham Patel <prathampatel@thefossguy.com>
2026-09-23 13:44:38 +0530

    systems/indra: add kmod: ch341

--- nixos-configuration/systems/indra/default.nix
+++ nixos-configuration/systems/indra/default.nix
@@ -14,6 +14,7 @@
     blacklistedKernelModules = [ "snd_hda_codec_hdmi" ]; # we no wants sound over HDMI
     extraModprobeConfig = "options kvm_intel nested=1";
     kernelModules = [
+      "ch341"
       "rfcomm"
       "snd_hrtimer"
       "snd_seq_dummy"
